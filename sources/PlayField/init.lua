54fe3e0de1955b756902d7b4270ca05a7e6d64d3
semyon422 <semyon422@gmail.com>
2018-08-10 21:47:34 +0600

    Design improvements

--- sources/PlayField/init.lua
+++ sources/PlayField/init.lua
@@ -2,8 +2,6 @@ PlayField = createClass(soul.SoulObject)
 require("PlayField.StaticObject")
 require("PlayField.InputObject")
 
-PlayField.layer = 0.5
-
 PlayField.load = function(self)
 	self.filePath = self.directoryPath .. "/" .. self.fileName
 	self.cs = self.engine.noteSkin.cs
@@ -16,16 +14,6 @@ PlayField.load = function(self)
 	self.inputMode = self.engine.noteChart.inputMode:getString()
 	
 	self:loadObjects()
-	-- local fileName
-	-- local key = {
-		-- self.inputMode
-	-- }
-	-- local value = self.config:getKeyTable(key)[1]
-	-- if value then
-		-- fileName = value
-	-- else
-		-- return
-	-- end
 	
 	self.observer:subscribe(self.engine.observable)
 end
@@ -46,7 +34,7 @@ PlayField.loadStaticObjects = function(self, key, data)
 	for i = 1, #data do
 		local object = PlayField.StaticObject:new()
 		object.filePath = self.directoryPath .. "/" .. data[i]
-		object.layer = self.layer
+		object.layer = tonumber(self.config:getKeyTable({self.inputMode, "static", "layer"})[i]) or 0
 		object.cs = self.cs
 		object.x = self.config:getKeyTable({self.inputMode, "static", "x"})[i] or 0
 		object.y = self.config:getKeyTable({self.inputMode, "static", "y"})[i] or 0
@@ -54,6 +42,7 @@ PlayField.loadStaticObjects = function(self, key, data)
 		object.h = self.config:getKeyTable({self.inputMode, "static", "h"})[i] or 0
 		object:activate()
 		object.observer:subscribe(self.engine.observable)
+		table.insert(self.objects, object)
 	end
 end
 
@@ -63,7 +52,7 @@ PlayField.loadInputObjects = function(self, key, data)
 		object.filePathReleased = self.directoryPath .. "/" .. data[i]
 		object.filePathPressed = self.config:getKeyTable({self.inputMode, "input", key[3], "image", "pressed"})[i]
 		object.filePathPressed = object.filePathPressed and self.directoryPath .. "/" .. object.filePathPressed or object.filePathReleased
-		object.layer = self.layer
+		object.layer = tonumber(self.config:getKeyTable({self.inputMode, "input", key[3], "layer"})[i]) or 0
 		object.cs = self.cs
 		object.x = self.config:getKeyTable({self.inputMode, "input", key[3], "x"})[i] or 0
 		object.y = self.config:getKeyTable({self.inputMode, "input", key[3], "y"})[i] or 0
@@ -73,11 +62,12 @@ PlayField.loadInputObjects = function(self, key, data)
 		object.inputIndex = i
 		object:activate()
 		object.observer:subscribe(self.engine.observable)
+		table.insert(self.objects, object)
 	end
 end
 
 PlayField.unloadObjects = function(self)
-	for _, objects in ipairs(self.objects) do
+	for _, object in ipairs(self.objects) do
 		object:deactivate()
 	end
 end
