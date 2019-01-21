590021350c3ce06569db7a3c8e158e3716a41f56
semyon422 <semyon422@gmail.com>
2019-01-21 10:55:21 +0000

    Rewrite resource loader

--- sphere/game/NoteChartResourceLoader.lua
+++ sphere/game/NoteChartResourceLoader.lua
@@ -11,25 +11,46 @@ NoteChartResourceLoader.resourceNames = {}
 NoteChartResourceLoader.aliases = {}
 
 NoteChartResourceLoader.load = function(self, path, noteChart, callback)
-	self.path = path
 	local directoryPath = path:match("^(.+)/")
-	
 	if self.directoryPath and self.directoryPath ~= directoryPath then
 		self:unload()
-		self.directoryPath = directoryPath
 	end
+	self.directoryPath = directoryPath
+	self.path = path
+	self.noteChart = noteChart
+	self.callback = callback
 	
-	FileManager:addPath(directoryPath)
+	if self.noteChart.type == "bms" or self.noteChart.type == "osu" then
+		self:loadBMS()
+	elseif self.noteChart.type == "o2jam" then
+		self:loadOJM()
+	end
+end
+
+NoteChartResourceLoader.loadOJM = function(self)
+	local path = self.path:match("(.+)n/%d$") .. "m"
+	JamLoader:load(path, function(samples)
+		for name in pairs(samples) do
+			self.aliases[name] = path .. "/" .. name
+		end
+		self.callback()
+	end)
+end
+
+NoteChartResourceLoader.loadBMS = function(self)
+	FileManager:addPath(self.directoryPath)
 	
 	self.resourceCount = 0
 	self.resourceCountLoaded = 0
 	self.soundGroup = Group:new()
-	for resourceType, resourceName in noteChart:getResourceIterator() do
+	for resourceType, resourceName in self.noteChart:getResourceIterator() do
 		if resourceType == "sound" then
-			self:loadSound(resourceName)
-		elseif resourceType == "ojm" then
-			self.o2jam = true
-			self:loadOJM(callback)
+			local soundFilePath = FileManager:findFile(resourceName, "audio")
+			if soundFilePath then
+				self.soundGroup:add(soundFilePath)
+				self.resourceCount = self.resourceCount + 1
+				self.aliases[resourceName] = soundFilePath
+			end
 		end
 	end
 	
@@ -37,7 +58,7 @@ NoteChartResourceLoader.load = function(self, path, noteChart, callback)
 		return sound.load(soundFilePath, function()
 			self.resourceCountLoaded = self.resourceCountLoaded + 1
 			if self.resourceCountLoaded == self.resourceCount then
-				callback()
+				self.callback()
 			end
 			
 			return self.observable:send({
@@ -49,35 +70,23 @@ NoteChartResourceLoader.load = function(self, path, noteChart, callback)
 end
 
 NoteChartResourceLoader.unload = function(self)
-	FileManager:removePath(self.directoryPath)
-	
-	if self.o2jam then
-		JamLoader:unload(self.path:match("(.+)n/%d$") .. "m", function() end)
-		self.o2jam = false
+	if self.noteChart.type == "bms" or self.noteChart.type == "osu" then
+		self:unloadBMS()
+	elseif self.noteChart.type == "o2jam" then
+		self:unloadOJM()
 	end
+end
+
+NoteChartResourceLoader.unloadBMS = function(self)
+	FileManager:removePath(self.directoryPath)
 	
 	return self.soundGroup:call(function(soundFilePath)
 		return sound.unload(soundFilePath, function() end)
 	end)
 end
 
-NoteChartResourceLoader.loadSound = function(self, resourceName)
-	local soundFilePath = FileManager:findFile(resourceName, "audio")
-	if soundFilePath then
-		self.soundGroup:add(soundFilePath)
-		self.resourceCount = self.resourceCount + 1
-		self.aliases[resourceName] = soundFilePath
-	end
-end
-
-NoteChartResourceLoader.loadOJM = function(self, callback)
-	local path = self.path:match("(.+)n/%d$") .. "m"
-	JamLoader:load(path, function(samples)
-		for name in pairs(samples) do
-			self.aliases[name] = path .. "/" .. name
-		end
-		callback()
-	end)
+NoteChartResourceLoader.unloadOJM = function(self)
+	JamLoader:unload(self.path:match("(.+)n/%d$") .. "m", function() end)
 end
 
 return NoteChartResourceLoader
