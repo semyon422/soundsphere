1012edaae67a60ba8f007cdc018dbde68b272599
semyon422 <semyon422@gmail.com>
2019-03-16 17:10:09 +0000

    FullLongNote mod was added

--- sphere/game/modifiers/init.lua
+++ sphere/game/modifiers/init.lua
@@ -3,4 +3,5 @@ return {
 	require("sphere.game.modifiers.NoLongNote"),
 	require("sphere.game.modifiers.NoMeasureLine"),
 	require("sphere.game.modifiers.NoSpeedVariation"),
+	require("sphere.game.modifiers.FullLongNote"),
 }
