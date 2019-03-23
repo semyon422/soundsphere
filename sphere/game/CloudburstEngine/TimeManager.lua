42085cf846bfcea1444c28e0835779c4685fdce3
semyon422 <semyon422@gmail.com>
2019-03-23 15:31:36 +0000

    Round current time to nearest time point if it is in frame

--- sphere/game/CloudburstEngine/TimeManager.lua
+++ sphere/game/CloudburstEngine/TimeManager.lua
@@ -10,6 +10,63 @@ TimeManager.load = function(self)
 	self.rateDelta = 0
 	self.state = "waiting"
 	self.playState = "delayed"
+	
+	self:loadTimePoints()
+end
+
+TimeManager.loadTimePoints = function(self)
+	local absoluteTimes = {}
+
+	local noteChart = self.engine.noteChart
+	for layerIndex in noteChart:getLayerDataIndexIterator() do
+		local timePointList = noteChart:requireLayerData(layerIndex).timeData.timePointList
+		for timePointIndex = 1, #timePointList do
+			local timePoint = timePointList[timePointIndex]
+			absoluteTimes[timePoint.absoluteTime] = true
+		end
+	end
+	
+	local absoluteTimeList = {}
+	for time in pairs(absoluteTimes) do
+		absoluteTimeList[#absoluteTimeList + 1] = time
+	end
+	table.sort(absoluteTimeList)
+	
+	self.absoluteTimeList = absoluteTimeList
+	self.nextTimeIndex = 1
+end
+
+TimeManager.updateNextTimeIndex = function(self)
+	local timeList = self.absoluteTimeList
+	while true do
+		if
+			timeList[self.nextTimeIndex + 1] and
+			self.currentTime >= timeList[self.nextTimeIndex]
+		then
+			self.nextTimeIndex = self.nextTimeIndex + 1
+		else
+			break
+		end
+	end
+end
+
+TimeManager.getNearestTime = function(self)
+	local timeList = self.absoluteTimeList
+	local prevTime = timeList[self.nextTimeIndex - 1]
+	local nextTime = timeList[self.nextTimeIndex]
+	
+	if not prevTime then
+		return nextTime
+	end
+	
+	local prevDelta = math.abs(self.currentTime - prevTime)
+	local nextDelta = math.abs(self.currentTime - nextTime)
+	
+	if prevDelta < nextDelta then
+		return prevTime
+	else
+		return nextTime
+	end
 end
 
 TimeManager.update = function(self)
@@ -33,6 +90,8 @@ TimeManager.update = function(self)
 	elseif self.state == "ended" then
 		self.currentTime = (deltaTime - self.pauseTime - self.rateDelta) * self.rate
 	end
+	
+	self:updateNextTimeIndex()
 end
 
 TimeManager.unload = function(self)
@@ -54,7 +113,12 @@ TimeManager.setRate = function(self, rate)
 end
 
 TimeManager.getCurrentTime = function(self)
-	return self.currentTime
+	local nearestTime = self:getNearestTime()
+	if math.abs(self.currentTime - nearestTime) < math.min(love.timer.getDelta() / 2, 1 / 30) then
+		return nearestTime
+	else
+		return self.currentTime
+	end
 end
 
 TimeManager.pause = function(self)
