2d5696e11d96b7ff73bc2f64236b0dd053b88113
semyon422 <semyon422@users.noreply.github.com>
2019-07-17 20:26:14 +0500

    Add fonts

--- sphere/assets/fonts.lua
+++ sphere/assets/fonts.lua
@@ -3,4 +3,19 @@ local fonts = {}
 fonts.NotoSansRegular = "resources/fonts/NotoSansCJK-Regular.ttc"
 fonts.NotoMonoRegular = "resources/fonts/NotoMono-Regular.ttf"
 
+fonts.SourceCodeProBlack = "resources/fonts/SourceCodePro-Black.ttf"
+fonts.SourceCodeProBlackIt = "resources/fonts/SourceCodePro-BlackIt.ttf"
+fonts.SourceCodeProBold = "resources/fonts/SourceCodePro-Bold.ttf"
+fonts.SourceCodeProBoldIt = "resources/fonts/SourceCodePro-BoldIt.ttf"
+fonts.SourceCodeProExtraLight = "resources/fonts/SourceCodePro-ExtraLight.ttf"
+fonts.SourceCodeProExtraLightIt = "resources/fonts/SourceCodePro-ExtraLightIt.ttf"
+fonts.SourceCodeProIt = "resources/fonts/SourceCodePro-It.ttf"
+fonts.SourceCodeProLight = "resources/fonts/SourceCodePro-Light.ttf"
+fonts.SourceCodeProLightIt = "resources/fonts/SourceCodePro-LightIt.ttf"
+fonts.SourceCodeProMedium = "resources/fonts/SourceCodePro-Medium.ttf"
+fonts.SourceCodeProMediumIt = "resources/fonts/SourceCodePro-MediumIt.ttf"
+fonts.SourceCodeProRegular = "resources/fonts/SourceCodePro-Regular.ttf"
+fonts.SourceCodeProSemibold = "resources/fonts/SourceCodePro-Semibold.ttf"
+fonts.SourceCodeProSemiboldIt = "resources/fonts/SourceCodePro-SemiboldIt.ttf"
+
 return fonts
