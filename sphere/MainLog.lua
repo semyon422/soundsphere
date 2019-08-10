e521d0e7308e33ad10ba3d948e2afce4989d7cc5
semyon422 <semyon422@users.noreply.github.com>
2019-08-10 19:43:48 +0500

    Add thread errors to main log

--- sphere/MainLog.lua
+++ sphere/MainLog.lua
@@ -1,8 +1,17 @@
 local Log = require("aqua.util.Log")
+local ThreadPool = require("aqua.thread.ThreadPool")
 
 local MainLog = Log:new()
 
 MainLog.console = true
 MainLog.path = "userdata/main.log"
+
+MainLog.receive = function(self, event)
+	if event.name == "threadError" then
+		self:write("error", event.error)
+	end
+end
+
+ThreadPool.observable:add(MainLog)
 
 return MainLog
