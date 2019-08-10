eea006dd0c5db26787ec0c725d661e165d34630a
semyon422 <semyon422@users.noreply.github.com>
2019-08-10 20:35:06 +0500

    Fix metadata in ToOsu mod

--- sphere/game/ModifierManager/ToOsu.lua
+++ sphere/game/ModifierManager/ToOsu.lua
@@ -6,8 +6,11 @@ local ToOsu = Modifier:new()
 ToOsu.name = "ToOsu"
 
 ToOsu.apply = function(self)
+	local GameplayScreen = require("sphere.screen.GameplayScreen")
+	
 	local nce = NoteChartExporter:new()
 	nce.noteChart = self.noteChart
+	nce.cacheData = GameplayScreen.cacheData
 
 	local out = io.open("userdata/export/map.osu", "w")
 	out:write(nce:export())
