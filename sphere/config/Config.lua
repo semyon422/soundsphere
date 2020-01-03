9e4302329ca1446da0a73791b90f76d66b83c43c
semyon422 <semyon422@users.noreply.github.com>
2020-01-03 12:51:33 +0500

    Disable tps limit slider

--- sphere/config/Config.lua
+++ sphere/config/Config.lua
@@ -65,7 +65,7 @@ Config.defaultValues = {
 	
 	["speed"] = 1,
 	["fps"] = 240,
-	["tps"] = 240,
+	-- ["tps"] = 240,
 	
 	["volume.global"] = 1,
 	["volume.music"] = 1,
--- sphere/screen/settings/SettingsList.lua
+++ sphere/screen/settings/SettingsList.lua
@@ -95,17 +95,17 @@ SettingsList.setGeneralItems = function(self)
 		step = 10,
 		format = "%d"
 	}
-	items[#items + 1] = {
-		name = "TPS limit",
-		configKey = "tps",
-		type = "slider",
-		minValue = 10,
-		maxValue = 1000,
-		minDisplayValue = 10,
-		maxDisplayValue = 1000,
-		step = 10,
-		format = "%d"
-	}
+	-- items[#items + 1] = {
+	-- 	name = "TPS limit",
+	-- 	configKey = "tps",
+	-- 	type = "slider",
+	-- 	minValue = 10,
+	-- 	maxValue = 1000,
+	-- 	minDisplayValue = 10,
+	-- 	maxDisplayValue = 1000,
+	-- 	step = 10,
+	-- 	format = "%d"
+	-- }
 	items[#items + 1] = {
 		name = "play speed",
 		configKey = "speed",
