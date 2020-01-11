2c9b926588df7da3f9c6a37c18316312c1b0a31e
semyon422 <semyon422@users.noreply.github.com>
2020-01-12 00:35:07 +0500

    Add more types of brackets to BMS name detection function

--- sphere/database/CacheDataFactory.lua
+++ sphere/database/CacheDataFactory.lua
@@ -77,6 +77,10 @@ local trimName = function(name)
 		return name:match("\"(.+)\""), name:find("\".+\"$")
 	elseif name:find("〔.+〕$") then
 		return name:match("〔(.+)〕"), name:find("〔.+〕$")
+	elseif name:find("‾.+‾$") then
+		return name:match("‾(.+)‾"), name:find("‾.+‾$")
+	elseif name:find("~.+~$") then
+		return name:match("~(.+)~"), name:find("~.+~$")
 	else
 		return name, #name + 1
 	end
