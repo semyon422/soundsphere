d9f20a3afe02bb6a91aaef8c986e5e5294cc3607
semyon422 <semyon422@users.noreply.github.com>
2020-05-29 22:06:36 +0500

    Add level display

--- sphere/screen/select/NoteChartDataDisplay.lua
+++ sphere/screen/select/NoteChartDataDisplay.lua
@@ -4,13 +4,14 @@ local NoteChartDataDisplay = Button:new()
 
 NoteChartDataDisplay.loadGui = function(self)
 	self.field = self.data.field
+	self.format = self.data.format
 
 	Button.loadGui(self)
 end
 
 NoteChartDataDisplay.receive = function(self, event)
 	if event.action == "updateMetaData" then
-		self.text = event.noteChartDataEntry[self.field] or ""
+		self.text = self.format:format(event.noteChartDataEntry[self.field] or "")
 		self:reload()
 	end
 	Button.receive(self, event)
