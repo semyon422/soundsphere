c2fbfa01abfca8c8b9cfffe7601f23c488a5f1d9
semyon422 <semyon422@users.noreply.github.com>
2020-07-16 18:51:47 +0500

    Allow to change scores.db schema

--- sphere/database/ScoreDatabase.lua
+++ sphere/database/ScoreDatabase.lua
@@ -27,6 +27,10 @@ ScoreDatabase.scoresNumberColumns = {
 }
 
 local createTableRequest = [[
+	CREATE TABLE IF NOT EXISTS `info` (
+		`key` NOT NULL PRIMARY KEY,
+		`value` TEXT NOT NULL
+	);
 	CREATE TABLE IF NOT EXISTS `scores` (
 		`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 		`noteChartHash` TEXT NOT NULL,
@@ -64,17 +68,72 @@ local selectScoresRequest = [[
 	SELECT * FROM `scores`;
 ]]
 
+local selectInfoRequest = [[
+	SELECT * FROM `info`;
+]]
+
+local insertInfoRequest = [[
+	INSERT OR IGNORE INTO `info` (`key`, `value`) VALUES (?, ?);
+]]
+
+local updateInfoRequest = [[
+	UPDATE `info` SET `value` = ? WHERE `key` = ?;
+]]
+
+local defaultInfo = {
+	version = 1
+}
+
 ScoreDatabase.load = function(self)
 	self.db = sqlite.open(self.dbpath)
-	
+
 	self.db:exec(createTableRequest)
-	
+
+	self.selectInfoStatement = self.db:prepare(selectInfoRequest)
+	self.insertInfoStatement = self.db:prepare(insertInfoRequest)
+	self.updateInfoStatement = self.db:prepare(updateInfoRequest)
+
+	self:insertDefaultInfo()
+	self:updateSchema()
+
 	self.insertScoreStatement = self.db:prepare(insertScoreRequest)
 	self.selectScoreStatement = self.db:prepare(selectScoreRequest)
 
 	self.selectScoresStatement = self.db:prepare(selectScoresRequest)
 
 	self.loaded = true
+end
+
+ScoreDatabase.selectInfo = function(self)
+	local info = {}
+
+	local stmt = self.selectInfoStatement:reset()
+	local row = stmt:step()
+	while row do
+		info[row[1]] = tonumber(row[2]) or row[2] or ""
+
+		row = stmt:step()
+	end
+
+	return info
+end
+
+ScoreDatabase.insertInfo = function(self, key, value)
+	return self.insertInfoStatement:reset():bind(
+		key, value
+	):step()
+end
+
+ScoreDatabase.updateInfo = function(self, key, value)
+	return self.updateInfoStatement:reset():bind(
+		value, key
+	):step()
+end
+
+ScoreDatabase.insertDefaultInfo = function(self)
+	for key, value in pairs(defaultInfo) do
+		self:insertInfo(key, value)
+	end
 end
 
 ScoreDatabase.unload = function(self)
@@ -112,6 +171,42 @@ end
 
 ScoreDatabase.transformScoreEntry = function(self, entry)
 	return self:transformEntry(entry, self.scoresColumns, self.scoresNumberColumns)
+end
+
+local updates = {}
+
+updates[1] = [[
+	ALTER TABLE scores RENAME TO temp;
+	CREATE TABLE IF NOT EXISTS `scores` (
+		`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		`noteChartHash` TEXT NOT NULL,
+		`noteChartIndex` REAL NOT NULL,
+		`playerName` TEXT,
+		`time` INTEGER,
+		`score` REAL,
+		`accuracy` REAL,
+		`maxCombo` INTEGER,
+		`scoreRating` REAL,
+		`mods` TEXT
+	);
+	INSERT INTO scores(id, noteChartHash, noteChartIndex, playerName, time, score, accuracy, maxCombo, scoreRating, mods)
+	SELECT id, noteChartHash, noteChartIndex, playerName, time, score, accuracy, maxCombo, scoreRating, mods FROM temp;
+	DROP TABLE temp;
+]]
+
+ScoreDatabase.updateSchema = function(self)
+	local info = self:selectInfo()
+
+	if info.version > defaultInfo.version then
+		error("you can not use newer score database in older game versions")
+	end
+
+	while info.version < defaultInfo.version do
+		info.version = info.version + 1
+		self.db:exec(updates[info.version])
+		print("schema was updated")
+	end
+	self:updateInfo("version", defaultInfo.version)
 end
 
 return ScoreDatabase
