c9665406dfd64e06c929780622a1cddd98cbc522
semyon422 <semyon422@users.noreply.github.com>
2020-10-18 17:07:23 +0500

    Add more debug messages

--- sphere/models/OnlineModel/NoteChartSubmitter.lua
+++ sphere/models/OnlineModel/NoteChartSubmitter.lua
@@ -33,12 +33,19 @@ NoteChartSubmitter.submitNoteChart = function(self, noteChartEntry, url)
             local noteChartFile = love.filesystem.newFile(path, "r")
             local content = noteChartFile:read()
             local tempName = "nc" .. os.time()
-            local tempFile = io.open(tempName, "wb")
+            local tempFile, err = io.open(tempName, "wb")
+            if not tempFile then
+                print("Can't create temporary file " .. tempName)
+                print(err)
+            else
+                print("Created temporary file " .. tempName)
+            end
             tempFile:write(content)
             tempFile:close()
 
             local request = require("luajit-request")
 
+            print("POST " .. data.host .. "/" .. data.url)
             local result, err, message = request.send(data.host .. "/" .. data.url, {
                 method = "POST",
                 files = {
@@ -48,14 +55,14 @@ NoteChartSubmitter.submitNoteChart = function(self, noteChartEntry, url)
 
             if not result then
                 print(err, message)
+            else
+                print(result.body)
+                
+                thread:push({
+                    name = "NoteChartSubmitResponse",
+                    body = result.body
+                })
             end
-
-            print(result.body)
-            
-            thread:push({
-				name = "NoteChartSubmitResponse",
-				body = result.body
-            })
             
             os.remove(tempName)
 		]],
