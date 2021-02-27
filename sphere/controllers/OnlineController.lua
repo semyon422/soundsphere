f5033d020578d42db122ad445173b0bc4e3b6204
semyon422 <semyon422@users.noreply.github.com>
2021-02-27 21:51:14 +0500

    Fix OnlineController crash

--- sphere/controllers/OnlineController.lua
+++ sphere/controllers/OnlineController.lua
@@ -7,11 +7,12 @@ OnlineController.construct = function(self)
 end
 
 OnlineController.load = function(self)
-	local token = self.configModel:get("online.token")
+	local config = self.configModel:getConfig("settings")
+	local token = config.online.token
 	if #token == 0 then
 		print("creating new token")
-		local email = self.configModel:get("online.email")
-		local password = self.configModel:get("online.password")
+		local email = config.online.email
+		local password = config.online.password
 		self.onlineModel:createToken(email, password)
 		return
 	end
@@ -20,6 +21,7 @@ OnlineController.load = function(self)
 end
 
 OnlineController.receive = function(self, event)
+	local config = self.configModel:getConfig("settings")
 	if event.name == "ScoreSubmitResponse" then
 		print(event.response.message)
 		print("Server received the score")
@@ -43,21 +45,19 @@ OnlineController.receive = function(self, event)
 		print("Server received the replay")
 	elseif event.name == "TokenResponse" then
 		if event.response.status then
-			self.configModel:set("online.token", event.response.token)
+			config.online.token = event.response.token
 			print("New token: " .. event.response.token)
 			print("check session")
 			self.onlineModel:checkSession()
-			self.configModel:set("online.email", "")
-			self.configModel:set("online.password", "")
-			self.configModel:write()
+			config.online.email = ""
+			config.online.password = ""
 		else
 			print(event.response.message)
 		end
 	elseif event.name == "SessionResponse" then
 		if event.response.status then
-			self.configModel:set("online.session", event.response.session)
+			config.online.session = event.response.session
 			print("New session: " .. event.response.session)
-			self.configModel:write()
 			self.onlineModel:setSession(event.response.session)
 		else
 			print("Session was not created")
@@ -72,13 +72,13 @@ OnlineController.receive = function(self, event)
 		else
 			print("Current session is not valid")
 			print("create session")
-			self.onlineModel:createSession(self.configModel:get("online.token"))
+			self.onlineModel:createSession(config.online.token)
 		end
 	elseif event.name == "SessionUpdateResponse" then
 		print(event.response.message)
 	elseif event.name == "QuickLoginGetResponse" then
 		if event.response.status then
-			self.configModel:set("online.quick_login_key", event.response.key)
+			config.online.quick_login_key = event.response.key
 			local url = self.onlineModel.host .. "/quick_login?key=" .. event.response.key
 			print(url)
 			love.system.openURL(url)
@@ -89,11 +89,11 @@ OnlineController.receive = function(self, event)
 		if event.response.status then
 			event.name = "TokenResponse"
 			self:receive(event)
-			self.configModel:set("online.quick_login_key", "")
+			config.online.quick_login_key = ""
 		else
 			print(event.response.message)
 			print("Quick login key was deleted")
-			self.configModel:set("online.quick_login_key", "")
+			config.online.quick_login_key = ""
 		end
 	end
 end
@@ -110,7 +110,8 @@ OnlineController.submitReplay = function(self, replayHash, url)
 end
 
 OnlineController.update = function(self, dt)
-	local token = self.configModel:get("online.token")
+	local config = self.configModel:getConfig("settings")
+	local token = config.online.token
 	if #token == 0 then
 		return
 	end
