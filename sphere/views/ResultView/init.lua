95fe2a76fb21807d7c3d4aae44e850b9cc697c77
semyon422 <semyon422@users.noreply.github.com>
2021-04-02 21:38:41 +0500

    Add accuracy and point graphs to a result screen

--- sphere/views/ResultView/init.lua
+++ sphere/views/ResultView/init.lua
@@ -1,19 +1,34 @@
 local viewspackage = (...):match("^(.-%.views%.)")
 
-local Class = require("aqua.util.Class")
 local Node = require("aqua.util.Node")
 local ResultNavigator = require(viewspackage .. "ResultView.ResultNavigator")
 local BackgroundView = require(viewspackage .. "BackgroundView")
+
+local Class = require("aqua.util.Class")
+local ScoreView	= require("sphere.views.GameplayView.ScoreView")
+local PointGraphView	= require("sphere.views.GameplayView.PointGraphView")
+local ImageView	= require("sphere.views.GameplayView.ImageView")
+local SequenceView	= require("sphere.views.SequenceView")
 
 local ResultView = Class:new()
 
 ResultView.construct = function(self)
 	self.node = Node:new()
+	self.scoreView = ScoreView:new()
+	self.pointGraphView = PointGraphView:new()
+	self.imageView = ImageView:new()
+	self.sequenceView = SequenceView:new()
 end
 
 ResultView.load = function(self)
+	local scoreView = self.scoreView
+	local pointGraphView = self.pointGraphView
+	local imageView = self.imageView
+	local sequenceView = self.sequenceView
+	local configModel = self.configModel
+
 	local node = self.node
-	local config = self.configModel:getConfig("settings")
+	local config = configModel:getConfig("result")
 
 	local navigator = ResultNavigator:new()
 	self.navigator = navigator
@@ -23,27 +38,45 @@ ResultView.load = function(self)
 	local backgroundView = BackgroundView:new()
 	backgroundView.view = self
 
-	node:node(backgroundView)
+	scoreView.scoreSystem = self.scoreSystem
+	scoreView.noteChartModel = self.noteChartModel
+
+	pointGraphView.scoreSystem = self.scoreSystem
+	pointGraphView.noteChartModel = self.noteChartModel
+
+	imageView.root = "."
+
+	sequenceView:setView("ScoreView", scoreView)
+	sequenceView:setView("PointGraphView", pointGraphView)
+	sequenceView:setView("ImageView", imageView)
+	sequenceView:setSequenceConfig(config)
+	sequenceView:load()
+
+	-- node:node(backgroundView)
 
 	navigator:load()
 end
 
 ResultView.unload = function(self)
 	self.navigator:unload()
+	self.sequenceView:unload()
 end
 
 ResultView.receive = function(self, event)
 	self.node:callnext(event.name, event)
 	self.navigator:receive(event)
+	self.sequenceView:receive(event)
 end
 
 ResultView.update = function(self, dt)
 	self.node:callnext("update")
 	self.navigator:update()
+	self.sequenceView:update()
 end
 
 ResultView.draw = function(self)
 	self.node:callnext("draw")
+	self.sequenceView:draw()
 end
 
 return ResultView
