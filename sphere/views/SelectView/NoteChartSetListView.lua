c13c494d69e2a46162c01b0079173f8f63673f83
semyon422 <semyon422@users.noreply.github.com>
2021-05-05 21:20:32 +0500

    Add smooth scroll for chart set list

--- sphere/views/SelectView/NoteChartSetListView.lua
+++ sphere/views/SelectView/NoteChartSetListView.lua
@@ -1,5 +1,6 @@
 local viewspackage = (...):match("^(.-%.views%.)")
 
+local tween = require("tween")
 local Class = require("aqua.util.Class")
 local CoordinateManager = require("aqua.graphics.CoordinateManager")
 local NoteChartSetListItemView = require(viewspackage .. "SelectView.NoteChartSetListItemView")
@@ -10,10 +11,14 @@ NoteChartSetListView.construct = function(self)
 	self.itemView = NoteChartSetListItemView:new()
 	self.itemView.listView = self
 	self.cs = CoordinateManager:getCS(0.5, 0, 16 / 9 / 2, 0, "h")
+	self.stencilfunction = function()
+		self:drawStencil()
+	end
 end
 
 NoteChartSetListView.load = function(self)
-	self.state.selectedItem = 1
+	self.state.selectedItem = self.selectModel.noteChartSetItemIndex
+	self.state.selectedVisualItem = self.selectModel.noteChartSetItemIndex
 	self:reloadItems()
 end
 
@@ -42,21 +47,59 @@ NoteChartSetListView.receive = function(self, event)
 end
 
 NoteChartSetListView.update = function(self, dt)
-	self.state.selectedItem = self.selectModel.noteChartSetItemIndex
+	if self.state.selectedItem ~= self.selectModel.noteChartSetItemIndex then
+		self.state.scrollTween = tween.new(
+			0.1,
+			self.state,
+			{selectedVisualItem = self.selectModel.noteChartSetItemIndex},
+			"linear"
+		)
+		self.state.selectedItem = self.selectModel.noteChartSetItemIndex
+	end
+	if self.state.scrollTween then
+		self.state.scrollTween:update(dt)
+	end
+	if self.state.selectedVisualItem == self.state.selectedItem then
+		self.state.scrollTween = nil
+	end
 	self:reloadItems()
 end
 
+NoteChartSetListView.drawStencil = function(self)
+	local config = self.config
+	local cs = self.cs
+	local screen = config.screen
+
+	love.graphics.setColor(1, 1, 1, 1)
+	love.graphics.rectangle(
+		"fill",
+		cs:X(config.x / screen.h, true),
+		cs:Y(config.y / screen.h, true),
+		cs:X(config.w / screen.h),
+		cs:Y(config.h / screen.h)
+	)
+end
+
 NoteChartSetListView.draw = function(self)
+	love.graphics.stencil(
+		self.stencilfunction,
+		"replace",
+		1,
+		false
+	)
+	love.graphics.setStencilTest("greater", 0)
+
 	local state = self.state
 	local config = self.config
 
-	for i = 1, config.rows do
+	local deltaItemIndex = state.selectedItem - state.selectedVisualItem
+	for i = 0 - math.floor(deltaItemIndex), config.rows - math.floor(deltaItemIndex) do
 		local itemIndex = i + state.selectedItem - math.ceil(config.rows / 2)
+		local visualIndex = i + deltaItemIndex
 		local item = state.items[itemIndex]
 		if item then
 			local itemView = self.itemView
-			itemView.index = i
-			itemView.itemIndex = itemIndex
+			itemView.visualIndex = visualIndex
 			itemView.item = item
 			itemView.listView = self
 			itemView.prevItem = state.items[itemIndex - 1]
@@ -64,6 +107,8 @@ NoteChartSetListView.draw = function(self)
 			itemView:draw()
 		end
 	end
+
+	love.graphics.setStencilTest()
 end
 
 return NoteChartSetListView
