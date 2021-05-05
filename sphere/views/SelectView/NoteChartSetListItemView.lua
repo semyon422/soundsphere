29d00fe211b7f89bdd760b8089c3b7a2e3b681dc
semyon422 <semyon422@users.noreply.github.com>
2021-05-05 12:06:52 +0500

    Add tag circle code to NoteChartSetListItemView

--- sphere/views/SelectView/NoteChartSetListItemView.lua
+++ sphere/views/SelectView/NoteChartSetListItemView.lua
@@ -9,7 +9,8 @@ NoteChartSetListItemView.draw = function(self)
 	local cs = self.listView.cs
 	local screen = config.screen
 	local y = config.y + (self.index - 1) * config.h / config.rows
-	local noteChartDataEntry = self.item.noteChartDataEntries[1]
+	local item = self.item
+	local noteChartDataEntry = item.noteChartDataEntries[1]
 
 	love.graphics.setColor(1, 1, 1, 1)
 
@@ -39,6 +40,21 @@ NoteChartSetListItemView.draw = function(self)
 		cs.one / screen.h,
 		cs.one / screen.h
 	)
+
+	if item.tagged then
+		love.graphics.circle(
+			"line",
+			cs:X((config.x + config.point.x) / screen.h, true),
+			cs:Y((y + config.point.y) / screen.h, true),
+			cs:X(config.point.r / screen.h, true)
+		)
+		love.graphics.circle(
+			"fill",
+			cs:X((config.x + config.point.x) / screen.h, true),
+			cs:Y((y + config.point.y) / screen.h, true),
+			cs:X(config.point.r / screen.h, true)
+		)
+	end
 end
 
 return NoteChartSetListItemView
