39a756c80b075165d87acaac39341db913e41324
semyon422 <semyon422@users.noreply.github.com>
2021-05-07 20:08:40 +0500

    Update BackgroundView.lua

--- sphere/views/BackgroundView.lua
+++ sphere/views/BackgroundView.lua
@@ -1,26 +1,23 @@
 
-local Node = require("aqua.util.Node")
+local Class = require("aqua.util.Class")
 local frame_draw = require("aqua.graphics.frame_draw")
 local map = require("aqua.math").map
 local CoordinateManager = require("aqua.graphics.CoordinateManager")
 
-local BackgroundView = Node:new()
+local BackgroundView = Class:new()
 
-BackgroundView.parallax = 0.0125
-
-BackgroundView.load = function(self)
-	self:on("draw", self.draw)
-
+BackgroundView.construct = function(self)
 	self.cs = CoordinateManager:getCS(0, 0, 0, 0, "all")
 end
 
 BackgroundView.draw = function(self)
 	local cs = self.cs
+	local config = self.config
 
-	local images = self.view.backgroundModel.images
-	local alpha = self.view.backgroundModel.alpha
+	local images = self.backgroundModel.images
+	local alpha = self.backgroundModel.alpha
 
-	local r, g, b = 0.4, 0.4, 0.4
+	local r, g, b = config.dim, config.dim, config.dim
 
 	for i = 1, 3 do
 		if not images[i] then
@@ -35,14 +32,14 @@ BackgroundView.draw = function(self)
 			love.graphics.setColor(r, g, b, 0)
 		end
 
-		local mx = self.cs:x(love.mouse.getX(), true)
-		local my = self.cs:y(love.mouse.getY(), true)
+		local mx = cs:x(love.mouse.getX(), true)
+		local my = cs:y(love.mouse.getY(), true)
 		frame_draw(
 			images[i],
-			cs:X(0 - map(mx, 0, 1, self.parallax, 0), true),
-			cs:Y(0 - map(my, 0, 1, self.parallax, 0), true),
-			cs:X(1 + 2 * self.parallax),
-			cs:Y(1 + 2 * self.parallax),
+			cs:X(0 - map(mx, 0, 1, config.parallax, 0), true),
+			cs:Y(0 - map(my, 0, 1, config.parallax, 0), true),
+			cs:X(1 + 2 * config.parallax),
+			cs:Y(1 + 2 * config.parallax),
 			"out"
 		)
 	end
