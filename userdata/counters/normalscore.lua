bfa5afff6bc6ab85ed433f7451986e0ec60d3afe
semyon422 <semyon422@users.noreply.github.com>
2021-08-22 21:25:35 +0500

    Add adjusted normal score

--- userdata/counters/normalscore.lua
+++ userdata/counters/normalscore.lua
@@ -3,21 +3,27 @@ local normalscore = require("libchart.normalscore")
 local ns
 
 load = function()
+	scoreTable.normalscore_mean = 0
 	scoreTable.normalscore_accuracy = 0
 	scoreTable.normalscore_score = 0
+	scoreTable.normalscore_score_adjusted = 0
 	scoreTable.normalscore_hit_count = 0
 	scoreTable.normalscore_miss_count = 0
 	scoreTable.normalscore_accuracy_sum = 0
+	scoreTable.normalscore_mean_sum = 0
 	ns = normalscore:new()
 end
 
 local increase = function(deltaTime, timeRate)
 	ns:hit(deltaTime, config.hitTimingWindow)
+	scoreTable.normalscore_mean = ns.mean
 	scoreTable.normalscore_accuracy = ns.score * 1e3
 	scoreTable.normalscore_score = ns.score * 1e6 / math.abs(timeRate)
+	scoreTable.normalscore_score_adjusted = ns.score_adjusted * 1e6 / math.abs(timeRate)
 	scoreTable.normalscore_hit_count = ns.hit_count
 	scoreTable.normalscore_miss_count = ns.miss_count
 	scoreTable.normalscore_accuracy_sum = ns.accuracy_sum
+	scoreTable.normalscore_mean_sum = 0
 end
 
 receive = function(event)
