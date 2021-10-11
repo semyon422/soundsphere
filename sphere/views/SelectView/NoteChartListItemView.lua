c795b225c1aa346f4194748fb2146f98f8e8e6b4
semyon422 <semyon422@users.noreply.github.com>
2021-10-11 21:46:52 +0500

    Left click on chart to play

--- sphere/views/SelectView/NoteChartListItemView.lua
+++ sphere/views/SelectView/NoteChartListItemView.lua
@@ -1,5 +1,26 @@
+local transform = require("aqua.graphics.transform")
 local ListItemView = require("sphere.views.ListItemView")
 
 local NoteChartListItemView = ListItemView:new({construct = false})
+
+NoteChartListItemView.receive = function(self, event)
+	if event.name ~= "mousepressed" then
+		return
+	end
+
+	local config = self.listView.config
+
+	local x, y, w, h = self.listView:getItemPosition(self.itemIndex)
+	local tf = transform(config.transform):translate(config.x, config.y)
+	local mx, my = tf:inverseTransformPoint(love.mouse.getPosition())
+	tf:release()
+
+	if (mx >= x and mx <= x + w and my >= y and my <= y + h) then
+		local button = event.args[3]
+		if button == 1 then
+			self.listView.navigator:play()
+		end
+	end
+end
 
 return NoteChartListItemView
