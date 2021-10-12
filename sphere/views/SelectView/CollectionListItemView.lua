58d55b2c1e354ffda235251a82065803c0371a00
semyon422 <semyon422@users.noreply.github.com>
2021-10-12 16:38:28 +0500

    Delete old code

--- sphere/views/SelectView/CollectionListItemView.lua
+++ sphere/views/SelectView/CollectionListItemView.lua
@@ -1,33 +1,5 @@
-
-local transform = require("aqua.graphics.transform")
 local ListItemView = require("sphere.views.ListItemView")
 
 local CollectionListItemView = ListItemView:new({construct = false})
-
-CollectionListItemView.draw = function(self)
-	self.item.tagged = self.item == self.listView.state.selectedCollection
-
-	ListItemView.draw(self)
-end
-
-CollectionListItemView.receive = function(self, event)
-	if event.name ~= "mousepressed" then
-		return
-	end
-
-	local config = self.listView.config
-
-	local x, y, w, h = self.listView:getItemPosition(self.itemIndex)
-	local tf = transform(config.transform):translate(config.x, config.y)
-	local mx, my = tf:inverseTransformPoint(love.mouse.getPosition())
-	tf:release()
-
-	if (mx >= x and mx <= x + w and my >= y and my <= y + h) then
-		local button = event.args[3]
-		if button == 1 then
-			self.listView.navigator:setCollection(self.itemIndex)
-		end
-	end
-end
 
 return CollectionListItemView
