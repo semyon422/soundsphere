9191ad542c8b8022c1f9093303d2f1427f0ab7e4
semyon422 <semyon422@users.noreply.github.com>
2021-10-17 15:26:08 +0500

    Allow modifying base playfield elements

--- sphere/models/NoteSkinModel/BasePlayfield.lua
+++ sphere/models/NoteSkinModel/BasePlayfield.lua
@@ -2,7 +2,7 @@ local PlayfieldVsrg = require("sphere.models.NoteSkinModel.PlayfieldVsrg")
 
 local BasePlayfield = PlayfieldVsrg:new({construct = false})
 
-BasePlayfield.addBaseElements = function(self)
+BasePlayfield.addBaseProgressBar = function(self)
 	self:addProgressBar({
 		x = 0,
 		y = 1070,
@@ -13,7 +13,9 @@ BasePlayfield.addBaseElements = function(self)
 		direction = "left-right",
 		mode = "+"
 	})
+end
 
+BasePlayfield.addBaseHpBar = function(self)
 	self:addHpBar({
 		x = 0,
 		y = 0,
@@ -24,7 +26,9 @@ BasePlayfield.addBaseElements = function(self)
 		direction = "down-up",
 		mode = "+"
 	})
+end
 
+BasePlayfield.addBaseScore = function(self)
 	self:addScore({
 		x = 0,
 		baseline = 52,
@@ -36,7 +40,9 @@ BasePlayfield.addBaseElements = function(self)
 		},
 		transform = self:newTransform(1920, 1080, "right")
 	})
+end
 
+BasePlayfield.addBaseAccuracy = function(self)
 	self:addAccuracy({
 		x = 0,
 		baseline = 94,
@@ -48,7 +54,9 @@ BasePlayfield.addBaseElements = function(self)
 		},
 		transform = self:newTransform(1920, 1080, "right")
 	})
+end
 
+BasePlayfield.addBaseCombo = function(self)
 	self:addCombo({
 		x = -540,
 		baseline = 476,
@@ -61,6 +69,30 @@ BasePlayfield.addBaseElements = function(self)
 		transform = self:newLaneCenterTransform(1080),
 		color = {1, 1, 1, 0.4},
 	})
+end
+
+BasePlayfield.addBaseElements = function(self, elements)
+	if not elements then
+		self:addBaseProgressBar()
+		self:addBaseHpBar()
+		self:addBaseScore()
+		self:addBaseAccuracy()
+		self:addBaseCombo()
+		return
+	end
+	for i, element in ipairs(elements) do
+		if element == "progress" then
+			self:addBaseProgressBar()
+		elseif element == "hp" then
+			self:addBaseHpBar()
+		elseif element == "score" then
+			self:addBaseScore()
+		elseif element == "accuracy" then
+			self:addBaseAccuracy()
+		elseif element == "combo" then
+			self:addBaseCombo()
+		end
+	end
 end
 
 return BasePlayfield
