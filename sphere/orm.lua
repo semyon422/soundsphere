0646287fb0b8c0e74045534f7a50a6292bd0c165
semyon422 <semyon422@users.noreply.github.com>
2022-03-10 14:34:27 +0500

    Minor fixes in orm.lua

--- sphere/orm.lua
+++ sphere/orm.lua
@@ -13,24 +13,36 @@ function orm:open(db)
 	self.c = sqlite.open(db)
 end
 
-function orm:close(db)
+function orm:close()
 	return self.c:close()
 end
 
 function orm:begin()
-	return self.c:exec("BEGIN;")
+	return self.c:exec("BEGIN")
 end
 
 function orm:commit()
-	return self.c:exec("COMMIT;")
+	return self.c:exec("COMMIT")
 end
 
-function orm:toobject(object, row, colnames)
+local function to_object(object, row, colnames)
 	object = object or {}
 	for i, k in ipairs(colnames) do
-		object[k] = row[i]
+		local value = row[i]
+		if type(value) == "cdata" then
+			value = tonumber(value) or value
+		end
+		object[k] = value
 	end
 	return object
+end
+
+local function escape_identifier(s)
+	if type(s) == "table" then
+		return s[1]
+	end
+	s = tostring(s)
+	return '`' .. (s:gsub('`', '``')) .. '`'
 end
 
 function orm:query(query, ...)
@@ -47,31 +59,32 @@ function orm:query(query, ...)
 		return
 	end
 	while row do
-		print(unpack(row))
-		objects[#objects + 1] = orm:toobject({}, row, colnames)
+		objects[#objects + 1] = to_object({}, row, colnames)
 		row = stmt:step(row)
 	end
 	return objects
 end
 
 function orm:select(query, ...)
-	return self:query("select " .. query, ...) or {}
+	return self:query("SELECT " .. query, ...) or {}
 end
 
 function orm:update(table_name, values, conditions, ...)
 	local assigns = {}
 	for k, v in pairs(values) do
-		table.insert(assigns, ("%s = %q"):format(k, v))
+		table.insert(assigns, ("%s = %q"):format(
+			escape_identifier(k), v
+		))
 	end
 
-	self:query(("update %q set %s where %s returning *"):format(
-		table_name, table.concat(assigns, ", "), conditions
+	self:query(("UPDATE %s SET %s WHERE %s"):format(
+		escape_identifier(table_name), table.concat(assigns, ", "), conditions
 	), ...)
 end
 
 function orm:delete(table_name, conditions, ...)
-	self:query(("delete from %q where %s"):format(
-		table_name, conditions
+	self:query(("DELETE FROM %s WHERE %s"):format(
+		escape_identifier(table_name), conditions
 	), ...)
 end
 
@@ -80,34 +93,20 @@ function orm:insert(table_name, values)
 	local keys = {}
 	for key in pairs(values) do
 		count = count + 1
-		keys[count] = key
+		keys[count] = escape_identifier(key)
 	end
 	local pattern = ("(%s)"):format(("?, "):rep(count - 1) .. "?")
 
-	local stmt = self.c:prepare(("insert into %q %s values %s returning *"):format(
-		table_name, ("(%s)"):format(table.concat(keys, ", ")), pattern
+	local stmt = self.c:prepare(("INSERT INTO %s %s VALUES %s RETURNING *"):format(
+		escape_identifier(table_name), ("(%s)"):format(table.concat(keys, ", ")), pattern
 	))
 
-	local i = 1
-	for _, key in ipairs(keys) do
+	for i, key in ipairs(keys) do
 		stmt:bind1(i, values[key])
-		i = i + 1
 	end
+
 	local row, colnames = stmt:step({}, {})
-
-	local types = {}
-	for k, v in pairs(values) do
-		types[k] = type(v)
-	end
-	local object = orm:toobject(values, row, colnames)
-
-	for k, v in pairs(types) do
-		if v == "number" then
-			object[k] = tonumber(object[k])
-		end
-	end
-
-	return object
+	return to_object(values, row, colnames)
 end
 
 return orm
