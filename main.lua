76042d4f663fb027558e3c04525df9657e6f168d
semyon422 <semyon422@users.noreply.github.com>
2022-03-16 16:08:37 +0500

    Use absolute path for LD_LIBRARY_PATH and PATH, use chdir

--- main.lua
+++ main.lua
@@ -20,18 +20,23 @@ aquapackage.add("serpent/src")
 
 local ffi = require("ffi")
 
+local source = love.filesystem.getSource()
+local sourceBase = love.filesystem.getSourceBaseDirectory()
+
 local jit_os = jit.os
 local arch = jit.arch
 if jit_os == "Windows" then
 	local bin = arch == "x64" and "bin/win64" or "bin/win32"
 	ffi.cdef("int _putenv_s(const char *varname, const char *value_string);")
-	ffi.C._putenv_s("PATH", os.getenv("PATH") .. ";" .. bin)
+	ffi.C._putenv_s("PATH", os.getenv("PATH") .. ";" .. sourceBase .. "/" .. bin)
 	aquapackage.add(bin)
 elseif jit_os == "Linux" then
 	local ldlp = os.getenv("LD_LIBRARY_PATH")
 	if not ldlp or not ldlp:find("bin/linux64") then
 		ffi.cdef("int setenv(const char *name, const char *value, int overwrite);")
-		ffi.C.setenv("LD_LIBRARY_PATH", (ldlp or "") .. ":bin/linux64", true)
+		ffi.cdef("int chdir(const char *path);")
+		ffi.C.setenv("LD_LIBRARY_PATH", (ldlp or "") .. ":" .. sourceBase .. "/bin/linux64", true)
+		ffi.C.chdir(sourceBase)
 		os.execute(arg[-2] .. " " .. arg[1] .. " &")
 		return os.exit()
 	end
@@ -41,8 +46,6 @@ love.window.setMode(1, 1)
 
 local aquafs = require("aqua.filesystem")
 
-local source = love.filesystem.getSource()
-local sourceBase = love.filesystem.getSourceBaseDirectory()
 local moddedgame = love.filesystem.getInfo("moddedgame")
 local moddedgamePrefix = ""
 if source:find("^.+%.love$") then
