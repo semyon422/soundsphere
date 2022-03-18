d5a9030c57902cdf3ce69565377609ab25505081
semyon422 <semyon422@users.noreply.github.com>
2022-03-18 21:38:59 +0500

    Update NoteChartLibraryModel.lua

--- sphere/models/NoteChartLibraryModel.lua
+++ sphere/models/NoteChartLibraryModel.lua
@@ -1,4 +1,5 @@
 local Class = require("aqua.util.Class")
+local CacheDatabase = require("sphere.models.CacheModel.CacheDatabase")
 
 local NoteChartLibraryModel = Class:new()
 
@@ -17,72 +18,48 @@ NoteChartLibraryModel.setNoteChartSetId = function(self, setId)
 end
 
 NoteChartLibraryModel.updateItems = function(self)
-	local items = {}
-	self.items = items
-
-	local noteChartEntries = self.cacheModel.cacheManager:getNoteChartsAtSet(self.setId)
-	if not noteChartEntries or not noteChartEntries[1] then
-		return items
-	end
-
-	local map = {}
-	local noteChartDataEntries = {}
-	for i = 1, #noteChartEntries do
-		local allEntries = self.cacheModel.cacheManager:getAllNoteChartDataEntries(noteChartEntries[i].hash)
-		if #allEntries == 0 then
-			allEntries = {self.cacheModel.cacheManager:getEmptyNoteChartDataEntry(noteChartEntries[i].path)}
-		end
-		for _, entry in pairs(allEntries) do
-			noteChartDataEntries[#noteChartDataEntries + 1] = entry
-			map[entry] = noteChartEntries[i]
-		end
-	end
-
-	local foundList, foundMap = self.searchModel:search(noteChartDataEntries)
-	for i = 1, #noteChartDataEntries do
-		local noteChartDataEntry = noteChartDataEntries[i]
-		local check = foundMap[noteChartDataEntry]
-		if check or self.searchMode == "show" then
-			items[#items + 1] = {
-				noteChartDataEntry = noteChartDataEntry,
-				noteChartEntry = map[noteChartDataEntry],
-				tagged = self.searchMode == "show" and check
-			}
-		end
-	end
-
-	table.sort(items, self.sortItemsFunction)
+	CacheDatabase:load()
+	self.items = CacheDatabase.db:query([[
+		SELECT noteChartDatas.*, noteChartDatas.id AS noteChartDataId, noteCharts.id AS noteChartId, noteCharts.path, noteCharts.setId,
+			CASE WHEN difficulty > 10 THEN TRUE
+			ELSE FALSE
+			END __boolean_tagged
+		FROM noteChartDatas
+		INNER JOIN noteCharts ON noteChartDatas.hash = noteCharts.hash
+		WHERE setId = ?
+		ORDER BY id
+	]], self.setId) or {}
+	CacheDatabase:unload()
 end
 
-NoteChartLibraryModel.sortItemsFunction = function(a, b)
-	a, b = a.noteChartDataEntry, b.noteChartDataEntry
-	if #a.inputMode ~= #b.inputMode then
-		return #a.inputMode < #b.inputMode
-	elseif a.inputMode ~= b.inputMode then
-		return a.inputMode < b.inputMode
-	elseif a.difficulty ~= b.difficulty then
-		return a.difficulty < b.difficulty
-	elseif a.name ~= b.name then
-		return a.name < b.name
-	end
-	return a.id < b.id
-end
+-- NoteChartLibraryModel.sortItemsFunction = function(a, b)
+-- 	a, b = a.noteChartDataEntry, b.noteChartDataEntry
+-- 	if #a.inputMode ~= #b.inputMode then
+-- 		return #a.inputMode < #b.inputMode
+-- 	elseif a.inputMode ~= b.inputMode then
+-- 		return a.inputMode < b.inputMode
+-- 	elseif a.difficulty ~= b.difficulty then
+-- 		return a.difficulty < b.difficulty
+-- 	elseif a.name ~= b.name then
+-- 		return a.name < b.name
+-- 	end
+-- 	return a.id < b.id
+-- end
 
 NoteChartLibraryModel.getItemIndex = function(self, noteChartEntryId, noteChartDataEntryId)
-	local items = self.items
-
-	if not items then
-		return 1
-	end
-
-	for i = 1, #items do
-		local item = items[i]
-		if item.noteChartEntry.id == noteChartEntryId and item.noteChartDataEntry.id == noteChartDataEntryId then
-			return i
-		end
-	end
-
-	return 1
+	CacheDatabase:load()
+	local result = CacheDatabase.db:query([[
+		SELECT * FROM
+		(
+			SELECT ROW_NUMBER() OVER(ORDER BY noteChartDatas.id) AS pos, noteCharts.id as ncId, noteChartDatas.id as ncdId, noteCharts.setId
+			FROM noteChartDatas
+			INNER JOIN noteCharts ON noteChartDatas.hash = noteCharts.hash
+			WHERE setId = ?
+		) A
+		WHERE setId = ? AND ncId = ? and ncdId = ?
+	]], self.setId, self.setId, noteChartEntryId, noteChartDataEntryId)
+	CacheDatabase:unload()
+	return result and result[1] and tonumber(result[1].pos) or 1
 end
 
 NoteChartLibraryModel.getItem = function(self, noteChartEntryId, noteChartDataEntryId)
