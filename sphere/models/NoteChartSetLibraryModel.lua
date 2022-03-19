1b6a633402b35126d5b87fed76387c46ce02b775
semyon422 <semyon422@users.noreply.github.com>
2022-03-19 12:20:56 +0500

    Implement pagination for NoteChartSetLibraryModel

--- sphere/models/NoteChartSetLibraryModel.lua
+++ sphere/models/NoteChartSetLibraryModel.lua
@@ -8,10 +8,58 @@ NoteChartSetLibraryModel.collapse = false
 
 NoteChartSetLibraryModel.construct = function(self)
 	self.items = {}
+	self.pages = {}
+	self.perPage = 10
 end
 
-NoteChartSetLibraryModel.updateItems = function(self)
-	self.items = CacheDatabase.db:query([[
+NoteChartSetLibraryModel.newItems = function(self)
+	return setmetatable({}, {__index = function(_, i)
+		return self:getPageItem(i)
+	end})
+end
+
+NoteChartSetLibraryModel.getPageNum = function(self, itemIndex)
+	local perPage = self.perPage
+	return
+		math.floor((itemIndex - 1) / perPage) + 1,
+		(itemIndex - 1) % perPage + 1
+end
+
+NoteChartSetLibraryModel.getPageItem = function(self, itemIndex)
+	local pageNum, pageItemIndex = self:getPageNum(itemIndex)
+	local currentPageNum = self:getPageNum(self.selectModel.noteChartSetItemIndex)
+	if math.abs(pageNum - currentPageNum) > 1 then
+		return
+	end
+
+	self:loadPage(pageNum)
+	self:unloadPages()
+	return self.pages[pageNum][pageItemIndex]
+end
+
+NoteChartSetLibraryModel.unloadPages = function(self)
+	local currentPageNum = self:getPageNum(self.selectModel.noteChartSetItemIndex)
+	local pages = self.pages
+	for pageNum in pairs(pages) do
+		if math.abs(pageNum - currentPageNum) > 1 then
+			pages[pageNum] = nil
+		end
+	end
+end
+
+NoteChartSetLibraryModel.loadPage = function(self, pageNum)
+	local pages = self.pages
+	if pages[pageNum] then
+		return
+	end
+
+	if pageNum <= 0 then
+		pages[pageNum] = {}
+		return
+	end
+
+	local perPage = self.perPage
+	pages[pageNum] = CacheDatabase.db:query([[
 		SELECT noteChartDatas.*, noteChartDatas.id AS noteChartDataId, noteCharts.id AS noteChartId, noteCharts.path, noteCharts.setId,
 			CASE WHEN difficulty > 10 THEN TRUE
 			ELSE FALSE
@@ -19,7 +67,27 @@ NoteChartSetLibraryModel.updateItems = function(self)
 		FROM noteChartDatas
 		INNER JOIN noteCharts ON noteChartDatas.hash = noteCharts.hash
 		ORDER BY id
-	]]) or {}
+		LIMIT ?
+		OFFSET ?
+	]], perPage, (pageNum - 1) * perPage) or {}
+end
+
+NoteChartSetLibraryModel.updateItems = function(self)
+	local itemsCount = CacheDatabase.db:query([[
+		SELECT COUNT(1) as c
+		FROM noteChartDatas
+		INNER JOIN noteCharts ON noteChartDatas.hash = noteCharts.hash
+	]])[1].c
+
+	self.items = newproxy(true)
+
+	local mt = getmetatable(self.items)
+	mt.__index = function(_, i)
+		return self:getPageItem(i)
+	end
+	mt.__len = function()
+		return itemsCount
+	end
 end
 
 NoteChartSetLibraryModel.getItemIndex = function(self, noteChartSetEntryId, noteChartEntryId, noteChartDataEntryId)
