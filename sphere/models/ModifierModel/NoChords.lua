bcbc16270595cf641262e23cd7279e144a0826fd
Emanuel-de-Jong <emanueldejong@hotmail.com>
2022-05-02 15:04:51 +0100

    Make NCH stepper instead and apply values.

--- sphere/models/ModifierModel/NoChords.lua
+++ sphere/models/ModifierModel/NoChords.lua
@@ -3,24 +3,40 @@ local Modifier = require("sphere.models.ModifierModel.Modifier")
 local NoChords = Modifier:new()
 
 NoChords.type = "NoteChartModifier"
-NoChords.interfaceType = "slider"
+NoChords.interfaceType = "stepper"
 
 NoChords.name = "NoChords"
 
-NoChords.defaultValue = 0
-NoChords.range = {0, 5}
+NoChords.defaultValue = "all"
+NoChords.range = {1, 9}
+NoChords.values = {"all", "0.25", "0.5", "0.75", "1", "2", "3", "4", "5"}
 
 NoChords.getString = function(self, config)
 	return "NCH"
 end
 
 NoChords.getSubString = function(self, config)
-	return config.value
+	local res = config.value
+	if res == "all" then
+		res = "A"
+	elseif res == "0.25" then
+		res = ".25"
+	elseif res == "0.5" then
+		res = ".5"
+	elseif res == "0.75" then
+		res = ".75"
+	end
+	return res
 end
 
 -- TODO: Also remove LN + ShortNote chords
 --		 and LN + LN chords
 NoChords.apply = function(self, config)
+	local configVal
+	if (config.value ~= "all") then
+		configVal = tonumber(config.value)
+	end
+
 	local noteChart = self.noteChartModel.noteChart
 	local layerDataSequence = noteChart.layerDataSequence
 	local inputCount = noteChart.inputMode:getInputCount("key")
@@ -68,31 +84,35 @@ NoChords.apply = function(self, config)
 		end
 		table.sort(sortedChords, function(a, b) return a.time < b.time end)
 
-		for _, chord in ipairs(sortedChords) do
-			table.sort(chord.notes, function(a, b) return a.inputIndex < b.inputIndex end)
-			local lowestSize = math.huge
-			local noteDataToKeep
-			for _, noteData in ipairs(chord.notes) do
-				local columnSize = chord.columnSizes[noteData.inputIndex]
-				if columnSize < lowestSize then
-					lowestSize = columnSize
-					noteDataToKeep = noteData
-				end
-			end
-
-			for _, noteData in pairs(chord.notes) do
-				if noteData ~= noteDataToKeep then
-					for _, futureChord in ipairs(sortedChords) do
-						futureChord.columnSizes[noteData.inputIndex] = futureChord.columnSizes[noteData.inputIndex] - 1
+		for chordCount, chord in ipairs(sortedChords) do
+			if configVal == nil or
+					(configVal >= 1 and chordCount % (configVal + 1) ~= 0) or
+					(configVal < 1 and (chordCount * 100) % (configVal * 100 + 100) == 0) then
+				table.sort(chord.notes, function(a, b) return a.inputIndex < b.inputIndex end)
+				local lowestSize = math.huge
+				local noteDataToKeep
+				for _, noteData in ipairs(chord.notes) do
+					local columnSize = chord.columnSizes[noteData.inputIndex]
+					if columnSize < lowestSize then
+						lowestSize = columnSize
+						noteDataToKeep = noteData
 					end
+				end
 
-					layerDataSequence:increaseInputCount(noteData.inputType, noteData.inputIndex, -1)
+				for _, noteData in pairs(chord.notes) do
+					if noteData ~= noteDataToKeep then
+						for _, futureChord in ipairs(sortedChords) do
+							futureChord.columnSizes[noteData.inputIndex] = futureChord.columnSizes[noteData.inputIndex] - 1
+						end
 
-					noteData.noteType = "SoundNote"
-					noteData.inputType = "auto"
-					noteData.inputIndex = 0
+						layerDataSequence:increaseInputCount(noteData.inputType, noteData.inputIndex, -1)
 
-					layerDataSequence:increaseInputCount(noteData.inputType, noteData.inputIndex, 1)
+						noteData.noteType = "SoundNote"
+						noteData.inputType = "auto"
+						noteData.inputIndex = 0
+
+						layerDataSequence:increaseInputCount(noteData.inputType, noteData.inputIndex, 1)
+					end
 				end
 			end
 		end
