24b270b76652c8351d568bccc05b7be99704afa4
semyon422 <semyon422@users.noreply.github.com>
2022-06-24 16:38:32 +0500

    Delete unused requires

--- sphere/views/HeaderViewConfig.lua
+++ sphere/views/HeaderViewConfig.lua
@@ -1,19 +1,5 @@
-
-local SequenceView = require("sphere.views.SequenceView")
-local ScrollBarView = require("sphere.views.ScrollBarView")
-local RectangleView = require("sphere.views.RectangleView")
-local CircleView = require("sphere.views.CircleView")
-local LineView = require("sphere.views.LineView")
 local UserInfoView = require("sphere.views.UserInfoView")
 local LogoView = require("sphere.views.LogoView")
-local ScreenMenuView = require("sphere.views.ScreenMenuView")
-local BackgroundView = require("sphere.views.BackgroundView")
-local ValueView = require("sphere.views.ValueView")
-local ImageView = require("sphere.views.ImageView")
-local CameraView = require("sphere.views.CameraView")
-local GaussianBlurView = require("sphere.views.GaussianBlurView")
-local ImageAnimationView = require("sphere.views.ImageAnimationView")
-local ImageValueView = require("sphere.views.ImageValueView")
 
 local transform = {{1 / 2, -16 / 9 / 2}, 0, 0, {0, 1 / 1080}, {0, 1 / 1080}, 0, 0, 0, 0}
 
