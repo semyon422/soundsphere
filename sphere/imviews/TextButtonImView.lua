53713d37b68c009916755116387e6a38ae9befe7
semyon422 <semyon422@users.noreply.github.com>
2022-09-02 16:45:24 +0500

    Redesign MountsView

--- sphere/imviews/TextButtonImView.lua
+++ sphere/imviews/TextButtonImView.lua
@@ -1,7 +1,7 @@
 local just = require("just")
 local just_print = require("just.print")
 
-return function(id, text, w, h)
+return function(id, text, w, h, align)
 	local mx, my = love.graphics.inverseTransformPoint(love.mouse.getPosition())
 	local over = 0 <= mx and mx <= w and 0 <= my and my <= h
 
@@ -13,7 +13,16 @@ return function(id, text, w, h)
 	end
 	love.graphics.setColor(1, 1, 1, 1)
 
-	just_print(text, 0, 0, w, h, "center", "center")
+	local font = love.graphics.getFont()
+	local fh = font:getHeight()
+	local p = 0
+
+	align = align or "center"
+	if align ~= "center" then
+		p = (h - fh) / 2
+	end
+
+	just_print(text, p, 0, w - p * 2, h, align, "center")
 
 	just.next(w, h)
 
--- sphere/views/MountsView.lua
+++ sphere/views/MountsView.lua
@@ -1,62 +1,80 @@
-local imgui = require("cimgui")
-local ffi = require("ffi")
-local align = require("aqua.imgui.config").align
+local just = require("just")
+local LabelImView = require("sphere.imviews.LabelImView")
+local HotkeyImView = require("sphere.imviews.HotkeyImView")
+local TextButtonImView = require("sphere.imviews.TextButtonImView")
+local TextButtonImView2 = require("sphere.imviews.TextButtonImView2")
 local ModalImView = require("sphere.imviews.ModalImView")
+local ContainerImView = require("sphere.imviews.ContainerImView")
+local ListImView = require("sphere.imviews.ListImView")
+local _transform = require("aqua.graphics.transform")
+local spherefonts = require("sphere.assets.fonts")
 
+local transform = {{1 / 2, -16 / 9 / 2}, 0, 0, {0, 1 / 1080}, {0, 1 / 1080}, 0, 0, 0, 0}
+
+local scrollY = 0
+local scrollYlist = 0
+
+local w, h = 768, 1080 / 2
+local _w, _h = w / 2, 55
+local r = 8
+local window_id = "MountsView"
 local selectedItem
-local isOpen = ffi.new("bool[1]", true)
-return ModalImView(function(self)
-	if not isOpen[0] then
-		isOpen[0] = true
-		return true
-	end
 
+return ModalImView(function(self)
 	local mountModel = self.game.mountModel
 	local items = self.game.configModel.configs.mount
 	selectedItem = selectedItem or items[1]
 
-	imgui.SetNextWindowPos({align(0.5, 279 + 454 * 3 / 4), 279}, 0)
-	imgui.SetNextWindowSize({454 * 1.5, 522}, 0)
-	local flags = imgui.love.WindowFlags("NoMove", "NoResize")
-	if imgui.Begin("Mounted directories", isOpen, flags) then
-		local avail = imgui.GetContentRegionAvail()
-		if imgui.BeginChild_Str("Mount points child window", {0, avail.y / 3}, false, 0) then
-			if imgui.BeginListBox("##Mount points", {-imgui.FLT_MIN, -imgui.FLT_MIN}) then
-				for i = 1, #items do
-					local item = items[i]
-					local isSelected = selectedItem == item
-					if imgui.Selectable_Bool(item[2], isSelected) then
-						selectedItem = item
-					end
+	love.graphics.setFont(spherefonts.get("Noto Sans", 24))
 
-					if isSelected then
-						imgui.SetItemDefaultFocus()
-					end
-				end
-				imgui.EndListBox()
-			end
+	love.graphics.replaceTransform(_transform(transform))
+	love.graphics.translate((1920 - w) / 2, (1080 - h) / 2)
+
+	love.graphics.setColor(0, 0, 0, 0.8)
+	love.graphics.rectangle("fill", 0, 0, w, h, r)
+	love.graphics.setColor(1, 1, 1, 1)
+
+	just.push()
+	ContainerImView(window_id, w, h, _h * 2, scrollY)
+
+	ListImView("mount points", w, h / 2, _h, scrollYlist)
+	for i = 1, #items do
+		local item = items[i]
+		local name = item[2]
+		if selectedItem == item then
+			name = "> " .. name
 		end
-		imgui.EndChild()
-		if selectedItem then
-			if imgui.BeginChild_Str("Mount point child window", {0, 0}, false, 0) then
-				imgui.Text("Status: " .. (mountModel.mountStatuses[selectedItem[1]] or "unknown"))
-				imgui.Text("Real path: ")
-				imgui.TextWrapped(selectedItem[1])
-				if imgui.Button("Open") then
-					love.system.openURL(selectedItem[1])
-				end
-				if imgui.Button("Remove") then
-					for i = 1, #items do
-						if items[i] == selectedItem then
-							table.remove(items, i)
-							selectedItem = nil
-							break
-						end
-					end
-				end
-			end
-			imgui.EndChild()
+		if TextButtonImView("mount item" .. i, name, w, _h, "left") then
+			selectedItem = item
 		end
 	end
-	imgui.End()
+	scrollYlist = ListImView()
+
+	if selectedItem then
+		just.indent(8)
+		just.text("Status: " .. (mountModel.mountStatuses[selectedItem[1]] or "unknown"))
+		just.indent(8)
+		just.text("Real path: ")
+		just.indent(8)
+		just.text(selectedItem[1], w)
+		if TextButtonImView2("open dir", "Open", 200, _h) then
+			love.system.openURL(selectedItem[1])
+		end
+		just.sameline()
+		if TextButtonImView2("remove dir", "Remove", 200, _h) then
+			for i = 1, #items do
+				if items[i] == selectedItem then
+					table.remove(items, i)
+					selectedItem = nil
+					break
+				end
+			end
+		end
+	end
+
+	scrollY = ContainerImView()
+	just.pop()
+
+	love.graphics.setColor(1, 1, 1, 1)
+	love.graphics.rectangle("line", 0, 0, w, h, r)
 end)
