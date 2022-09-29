4f3387d2a8f0328f38d390a69bea305cc4b98517
semyon422 <semyon422@users.noreply.github.com>
2022-09-29 15:57:32 +0500

    Refactor ListView

--- sphere/views/ListView.lua
+++ sphere/views/ListView.lua
@@ -9,12 +9,6 @@ ListView.targetItemIndex = 1
 ListView.itemIndex = 1
 ListView.visualItemIndex = 1
 
-ListView.forceScroll = function(self)
-	local itemIndex = assert(self:getItemIndex())
-	self.itemIndex = itemIndex
-	self.visualItemIndex = itemIndex
-end
-
 ListView.reloadItems = function(self)
 	self.stateCounter = 1
 	self.items = {}
@@ -49,35 +43,36 @@ ListView.draw = function(self)
 	local stateCounter = self.stateCounter
 	self:reloadItems()
 	if stateCounter ~= self.stateCounter then
-		self:forceScroll()
+		local itemIndex = assert(self:getItemIndex())
+		self.itemIndex = itemIndex
+		self.visualItemIndex = itemIndex
 	end
 
 	local tf = transform(self.transform):translate(self.x, self.y)
 	love.graphics.replaceTransform(tf)
 
-	local mx, my = love.graphics.inverseTransformPoint(love.mouse.getPosition())
-	local over = 0 <= mx and mx <= self.w and 0 <= my and my <= self.h
+	love.graphics.setColor(1, 1, 1, 1)
+	just.clip(love.graphics.rectangle, "fill", 0, 0, self.w, self.h)
 
-	local delta = just.wheel_over(self, over)
+	local h = self.h / self.rows
+	local visualItemIndex = self.visualItemIndex
+
+	local deltaItemIndex = math.floor(visualItemIndex) - visualItemIndex
+	love.graphics.translate(0, deltaItemIndex * h)
+
+	local delta = just.wheel_over(self, just.is_over(self.w, self.h))
 	if delta then
 		self:scroll(-delta)
 	end
 
-	love.graphics.setColor(1, 1, 1, 1)
-	just.clip(love.graphics.rectangle, "fill", 0, 0, self.w, self.h)
-
-	local deltaItemIndex = self.itemIndex - self.visualItemIndex
-	for i = 0 - math.floor(deltaItemIndex), self.rows - math.floor(deltaItemIndex) do
-		local itemIndex = i + self.itemIndex - math.ceil(self.rows / 2)
-		local item = self.items[itemIndex]
-		if item then
-			local visualIndex = math.ceil(self.rows / 2) + itemIndex - self.visualItemIndex
-			local h = self.h / self.rows
-
-			local tf = transform(self.transform):translate(self.x, self.y + (visualIndex - 1) * h)
-			love.graphics.replaceTransform(tf)
-			self:drawItem(itemIndex, self.w, h)
+	for i = math.floor(visualItemIndex), self.rows + math.ceil(visualItemIndex) - 1 do
+		local _i = i - math.floor(self.rows / 2)
+		if self.items[_i] then
+			just.push()
+			self:drawItem(_i, self.w, h)
+			just.pop()
 		end
+		just.emptyline(h)
 	end
 
 	just.clip()
