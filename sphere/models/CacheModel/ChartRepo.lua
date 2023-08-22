6b64b9f099b131ddd7e8d734c0bf2eb71bdf54bb
semyon422 <semyon422@users.noreply.github.com>
2023-08-22 15:30:16 +0500

    CacheRepo->ChartRepo

--- sphere/models/CacheModel/ChartRepo.lua
+++ sphere/models/CacheModel/ChartRepo.lua
@@ -2,13 +2,13 @@ local class = require("class")
 local Orm = require("sphere.Orm")
 local utf8 = require("utf8")
 
----@class sphere.CacheRepo
----@operator call: sphere.CacheRepo
-local CacheRepo = class()
+---@class sphere.ChartRepo
+---@operator call: sphere.ChartRepo
+local ChartRepo = class()
 
-CacheRepo.dbpath = "userdata/charts.db"
+ChartRepo.dbpath = "userdata/charts.db"
 
-function CacheRepo:load()
+function ChartRepo:load()
 	if self.loaded then
 		return
 	end
@@ -21,7 +21,7 @@ function CacheRepo:load()
 	db:exec(sql)
 end
 
-function CacheRepo:unload()
+function ChartRepo:unload()
 	if not self.loaded then
 		return
 	end
@@ -29,11 +29,11 @@ function CacheRepo:unload()
 	self.db:close()
 end
 
-function CacheRepo:begin()
+function ChartRepo:begin()
 	self.db:begin()
 end
 
-function CacheRepo:commit()
+function ChartRepo:commit()
 	self.db:commit()
 end
 
@@ -41,36 +41,36 @@ end
 
 ---@param entry table
 ---@return table
-function CacheRepo:insertNoteChartEntry(entry)
+function ChartRepo:insertNoteChartEntry(entry)
 	return self.db:insert("noteCharts", entry, true)
 end
 
 ---@param entry table
 ---@return table?
-function CacheRepo:updateNoteChartEntry(entry)
+function ChartRepo:updateNoteChartEntry(entry)
 	return self.db:update("noteCharts", entry, "path = ?", entry.path)
 end
 
 ---@param path string
 ---@return table?
-function CacheRepo:selectNoteChartEntry(path)
+function ChartRepo:selectNoteChartEntry(path)
 	return self.db:select("noteCharts", "path = ?", path)[1]
 end
 
 ---@param id number
 ---@return table?
-function CacheRepo:selectNoteChartEntryById(id)
+function ChartRepo:selectNoteChartEntryById(id)
 	return self.db:select("noteCharts", "id = ?", id)[1]
 end
 
 ---@param path string
-function CacheRepo:deleteNoteChartEntry(path)
+function ChartRepo:deleteNoteChartEntry(path)
 	self.db:delete("noteCharts", "path = ?", path)
 end
 
 ---@param setId number
 ---@return table
-function CacheRepo:getNoteChartsAtSet(setId)
+function ChartRepo:getNoteChartsAtSet(setId)
 	return self.db:select("noteCharts", "setId = ?", setId)
 end
 
@@ -78,36 +78,36 @@ end
 
 ---@param entry table
 ---@return table
-function CacheRepo:insertNoteChartSetEntry(entry)
+function ChartRepo:insertNoteChartSetEntry(entry)
 	return self.db:insert("noteChartSets", entry, true)
 end
 
 ---@param entry table
 ---@return table?
-function CacheRepo:updateNoteChartSetEntry(entry)
+function ChartRepo:updateNoteChartSetEntry(entry)
 	return self.db:update("noteChartSets", entry, "path = ?", entry.path)
 end
 
 ---@param path string
 ---@return table?
-function CacheRepo:selectNoteChartSetEntry(path)
+function ChartRepo:selectNoteChartSetEntry(path)
 	return self.db:select("noteChartSets", "path = ?", path)[1]
 end
 
 ---@param id number
 ---@return table?
-function CacheRepo:selectNoteChartSetEntryById(id)
+function ChartRepo:selectNoteChartSetEntryById(id)
 	return self.db:select("noteChartSets", "id = ?", id)[1]
 end
 
 ---@param path string
-function CacheRepo:deleteNoteChartSetEntry(path)
+function ChartRepo:deleteNoteChartSetEntry(path)
 	self.db:delete("noteChartSets", "path = ?", path)
 end
 
 ---@param path string
 ---@return table
-function CacheRepo:selectNoteChartSets(path)
+function ChartRepo:selectNoteChartSets(path)
 	return self.db:select("noteChartSets", "substr(path, 1, ?) = ?", utf8.len(path), path)
 end
 
@@ -115,27 +115,27 @@ end
 
 ---@param entry table
 ---@return table
-function CacheRepo:insertNoteChartDataEntry(entry)
+function ChartRepo:insertNoteChartDataEntry(entry)
 	return self.db:insert("noteChartDatas", entry, true)
 end
 
 ---@param entry table
 ---@return table?
-function CacheRepo:updateNoteChartDataEntry(entry)
+function ChartRepo:updateNoteChartDataEntry(entry)
 	return self.db:update("noteChartDatas", entry, "hash = ? and `index` = ?", entry.hash, entry.index)
 end
 
 ---@param hash string
 ---@param index number
 ---@return table?
-function CacheRepo:selectNoteCharDataEntry(hash, index)
+function ChartRepo:selectNoteCharDataEntry(hash, index)
 	return self.db:select("noteChartDatas", "hash = ? and `index` = ?", hash, index)[1]
 end
 
 ---@param id number
 ---@return table?
-function CacheRepo:selectNoteChartDataEntryById(id)
+function ChartRepo:selectNoteChartDataEntryById(id)
 	return self.db:select("noteChartDatas", "id = ?", id)[1]
 end
 
-return CacheRepo
+return ChartRepo
