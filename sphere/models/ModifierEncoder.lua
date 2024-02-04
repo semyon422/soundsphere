380568348b05404933f5cf1fa2d0c02bdabbac42
semyon422 <semyon422@users.noreply.github.com>
2024-02-04 19:18:18 +0500

    Save modifiers in db in a more compact form in lua

--- sphere/models/ModifierEncoder.lua
+++ sphere/models/ModifierEncoder.lua
@@ -1,6 +1,6 @@
 local class = require("class")
-local serpent = require("serpent")
 local json = require("json")
+local stbl = require("stbl")
 local md5 = require("md5")
 
 local ModifierEncoder = class()
@@ -8,13 +8,17 @@ local ModifierEncoder = class()
 ---@param config table
 ---@return string
 function ModifierEncoder:encode(config)
-	return json.encode(config)
+	local t = {}
+	for i, m in ipairs(config) do
+		t[i] = {m.id, m.version, m.value}
+	end
+	return stbl.encode(t)
 end
 
 ---@param config table
 ---@return string
 function ModifierEncoder:hash(config)
-	return md5.sumhexa(serpent.line(config, {sortkeys = true, comment = false, compact = true}))
+	return md5.sumhexa(stbl.encode(config))
 end
 
 ---@param s string
@@ -29,7 +33,19 @@ end
 ---@param s string
 ---@return table
 function ModifierEncoder:decode(s)
-	local ok, err = pcall(json.decode, s)
+	local ok, err = pcall(stbl.decode, s)
+	if ok then
+		local t = {}
+		for i, m in ipairs(err) do
+			t[i] = {
+				id = m[1],
+				version = m[2],
+				value = m[3],
+			}
+		end
+		return t
+	end
+	ok, err = pcall(json.decode, s)
 	if ok then
 		return err
 	end
