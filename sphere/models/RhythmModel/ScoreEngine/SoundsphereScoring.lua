9f5932335228eed09438ddfb3aa27665a86162ae
Thetan <83221369+Thetan-ILW@users.noreply.github.com>
2024-03-29 18:20:38 +0300

    Format

--- sphere/models/RhythmModel/ScoreEngine/SoundsphereScoring.lua
+++ sphere/models/RhythmModel/ScoreEngine/SoundsphereScoring.lua
@@ -1,94 +1,94 @@
-local ScoreSystem           = require("sphere.models.RhythmModel.ScoreEngine.ScoreSystem")
+local ScoreSystem = require("sphere.models.RhythmModel.ScoreEngine.ScoreSystem")
 
 ---@class sphere.SoundsphereScoring: sphere.ScoreSystem
 ---@operator call: sphere.SoundsphereScoring
-local SoundsphereScoring    = ScoreSystem + {}
+local SoundsphereScoring = ScoreSystem + {}
 
-SoundsphereScoring.name     = "soundsphere"
+SoundsphereScoring.name = "soundsphere"
 SoundsphereScoring.metadata = {
-	name = "Soundsphere",
+	name = "Soundsphere"
 }
 
-local orderedCounterNames   = { "perfect", "not perfect" }
+local orderedCounterNames = {"perfect", "not perfect"}
 
 function SoundsphereScoring:load()
-	self.judges = {
-		[self.metadata.name] = {
-			counters = {
+    self.judges = {
+        [self.metadata.name] = {
+            counters = {
 				perfect = 0,
 				["not perfect"] = 0,
 				miss = 0
 			},
-			earlyLate = {
+            earlyLate = {
 				early = 0,
 				late = 0
 			},
-			notes = 0,
-			getOrderedCounterNames = function()
-				return orderedCounterNames
-			end
-		},
-	}
+            notes = 0,
+            getOrderedCounterNames = function()
+                return orderedCounterNames
+            end
+        }
+    }
 end
 
 function SoundsphereScoring:hit(event)
-	local judge = self.judges[self.metadata.name]
+    local judge = self.judges[self.metadata.name]
 
-	judge.notes = judge.notes + 1
+    judge.notes = judge.notes + 1
 
-	local delta = event.deltaTime
+    local delta = event.deltaTime
 
-	if delta < 0 then
-		judge.earlyLate.early = judge.earlyLate.early + 1
-	else
-		judge.earlyLate.late = judge.earlyLate.late + 1
-	end
+    if delta < 0 then
+        judge.earlyLate.early = judge.earlyLate.early + 1
+    else
+        judge.earlyLate.late = judge.earlyLate.late + 1
+    end
 
-	if math.abs(delta) < 0.016 then
-		judge.counters.perfect = judge.counters.perfect + 1
-		return
-	end
+    if math.abs(delta) < 0.016 then
+        judge.counters.perfect = judge.counters.perfect + 1
+        return
+    end
 
-	judge.counters["not perfect"] = judge.counters["not perfect"] + 1
+    judge.counters["not perfect"] = judge.counters["not perfect"] + 1
 end
 
 function SoundsphereScoring:miss()
-	local judge = self.judges[self.metadata.name]
+    local judge = self.judges[self.metadata.name]
 
-	judge.notes = judge.notes + 1
-	judge.counters.miss = judge.counters.miss + 1
+    judge.notes = judge.notes + 1
+    judge.counters.miss = judge.counters.miss + 1
 end
 
 SoundsphereScoring.notes = {
-	ShortNote = {
+    ShortNote = {
 		clear = {
 			passed = "hit",
 			missed = "miss",
-			clear = nil,
-		},
+			clear = nil
+		}
 	},
-	LongNote = {
-		clear = {
-			startPassedPressed = "hit",
-			startMissed = "miss",
-			startMissedPressed = "miss",
-			clear = nil,
-		},
-		startPassedPressed = {
-			startMissed = "miss",
-			endMissed = "miss",
-			endPassed = "hit",
-		},
-		startMissedPressed = {
-			endMissedPassed = "hit",
-			startMissed = nil,
-			endMissed = "hit",
-		},
-		startMissed = {
+    LongNote = {
+        clear = {
+            startPassedPressed = "hit",
+            startMissed = "miss",
+            startMissedPressed = "miss",
+            clear = nil
+        },
+        startPassedPressed = {
+            startMissed = "miss",
+            endMissed = "miss",
+            endPassed = "hit"
+        },
+        startMissedPressed = {
+            endMissedPassed = "hit",
+            startMissed = nil,
+            endMissed = "hit"
+        },
+        startMissed = {
 			startMissedPressed = nil,
-			endMissed = "miss",
-		},
-	},
+			endMissed = "miss"
+		}
+    }
 }
 
 return SoundsphereScoring
