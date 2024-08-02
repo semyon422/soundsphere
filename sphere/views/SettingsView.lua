63aa8f47cc29958749a1a87e624076afff70afed
semyon422 <semyon422@users.noreply.github.com>
2024-08-02 20:33:35 +0500

    Remove calc top scores button

--- sphere/views/SettingsView.lua
+++ sphere/views/SettingsView.lua
@@ -431,9 +431,6 @@ function drawSection:misc()
 	if imgui.button("error button", "error") then
 		error("error")
 	end
-	if imgui.button("calc top scores", "calc top scores") then
-		self.game.scoreModel:asyncCalculateTopScores()
-	end
 
 	just.indent(8)
 	just.text("Commit: " .. version.commit)
