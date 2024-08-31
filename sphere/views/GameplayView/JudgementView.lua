f6cfa0f633b6b217ea2022f61508bd6043131ee3
semyon422 <semyon422@users.noreply.github.com>
2024-08-31 10:33:43 +0500

    Fix missing judgement image

--- sphere/views/GameplayView/JudgementView.lua
+++ sphere/views/GameplayView/JudgementView.lua
@@ -6,19 +6,20 @@ local inside = require("table_util").inside
 local JudgementView = class()
 
 function JudgementView:load()
-	self.judgement = nil
+	self.notes = 0
 end
 
 ---@param dt number
 function JudgementView:update(dt)
 	local judge = inside(self, self.key)
 
-	local judgement = judge.lastCounter
-	if not judgement or judgement == self.judgement then
+	local notes = judge.notes
+	if notes == self.notes then
 		return
 	end
-	self.judgement = judgement
+	self.notes = notes
 
+	local judgement = judge.lastCounter
 	for name, view in pairs(self.judgements) do
 		if name == judgement then
 			view:setTime(0)
