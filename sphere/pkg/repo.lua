1ae8c139ce27c47f0d26e09e26b0bad9d59500b8
semyon422 <semyon422@users.noreply.github.com>
2024-09-30 21:46:32 +0500

    Update repo.lua

--- sphere/pkg/repo.lua
+++ sphere/pkg/repo.lua
@@ -2,9 +2,21 @@
 local repo = {
 	{
 		name = "osu_ui",
-		display_name = "osu! ui",
+		display_name = "osu!",
 		url = "https://codeload.github.com/Thetan-ILW/osu_ui/zip/refs/heads/main",
 		source = "https://github.com/Thetan-ILW/osu_ui",
+	},
+	{
+		name = "msd_calculator",
+		display_name = "MSD Calculator",
+		url = "https://codeload.github.com/Thetan-ILW/MinaCalc-soundsphere/zip/refs/heads/main",
+		source = "https://github.com/Thetan-ILW/MinaCalc-soundsphere",
+	},
+	{
+		name = "player_profile",
+		display_name = "Local Player Profile",
+		url = "https://codeload.github.com/Thetan-ILW/PlayerProfile-soundsphere/zip/refs/heads/main",
+		source = "https://github.com/Thetan-ILW/PlayerProfile-soundsphere",
 	},
 }
 
