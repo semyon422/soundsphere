7aeaee4eecf7ecb5095d0e3bd7eab4a426ce3907
semyon422 <semyon422@users.noreply.github.com>
2025-03-05 23:32:47 +0500

    Fix role type in user_roles

--- sea/storage/server/models/user_roles.lua
+++ sea/storage/server/models/user_roles.lua
@@ -1,10 +1,13 @@
 local UserRole = require("sea.access.UserRole")
+local Roles = require("sea.access.Roles")
 
 ---@type rdb.ModelOptions
 local user_roles = {}
 
 user_roles.metatable = UserRole
 
-user_roles.types = {}
+user_roles.types = {
+	role = Roles.enum,
+}
 
 return user_roles
