46c5dba30b59f6d665961b1fec4a148bf2e3188e
Thetan <83221369+Thetan-ILW@users.noreply.github.com>
2025-03-29 11:28:43 +0300

    Add user description editor
    Put code in <script> tags inside functions

--- sea/access/http/UserResource.lua
+++ sea/access/http/UserResource.lua
@@ -1,5 +1,6 @@
 local IResource = require("web.framework.IResource")
 local UserPage = require("sea.access.http.UserPage")
+local http_util = require("http_util")
 
 ---@class sea.UserResource: web.IResource
 ---@operator call: sea.UserResource
@@ -24,9 +25,20 @@ end
 ---@param res web.IResponse
 ---@param ctx sea.RequestContext
 function UserResource:GET(req, res, ctx)
-	ctx.user = self.users:getUser(tonumber(ctx.path_params.user_id))
+	local query = http_util.decode_query_string(ctx.parsed_uri.query)
+
+	local user = self.users:getUser(tonumber(ctx.path_params.user_id))
+
+	if user.id == 0 then
+		self.views:render_send(res, "sea/shared/http/not_found.etlua", ctx, true)
+		return
+	end
+
+	ctx.user = user
 	ctx.page = UserPage(self.testActivity)
 	ctx.ignore_main_container = true
+	ctx.owner = ctx.user.id == ctx.session_user.id
+	ctx.edit_description = ctx.owner and query.edit_description == "true"
 	self.views:render_send(res, "sea/access/http/user.etlua", ctx, true)
 end
 
