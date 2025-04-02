f23302310a1a9138edb9187c8551425c8152dcca
semyon422 <semyon422@users.noreply.github.com>
2025-04-02 18:41:30 +0500

    Log icc errors in WebsocketResource

--- sea/shared/http/WebsocketResource.lua
+++ sea/shared/http/WebsocketResource.lua
@@ -42,17 +42,25 @@ function WebsocketResource:server(req, res, ctx)
 	local peer = WebsocketPeer(ws)
 	local task_handler = TaskHandler(self.remote_handler)
 
+	---@param msg icc.Message
+	local function handle_msg(msg)
+		if msg.ret then
+			task_handler:handleReturn(msg)
+		else
+			msg:insert(ctx.session_user, 3)
+			task_handler:handleCall(peer, msg)
+		end
+	end
+
 	function ws.protocol:text(payload, fin)
 		if not fin then return end
 
 		local msg = peer:decode(payload)
 		if not msg then return end
 
-		if msg.ret then
-			task_handler:handleReturn(msg)
-		else
-			msg:insert(ctx.session_user, 3)
-			task_handler:handleCall(peer, msg)
+		local ok, err = xpcall(handle_msg, debug.traceback, msg)
+		if not ok then
+			print("icc error ", err)
 		end
 	end
 
