ebd69980b7eb11637e5fac019a85804702ac17b7
semyon422 <semyon422@users.noreply.github.com>
2025-05-23 15:02:58 +0500

    Use correct count in TotalRating

--- sea/leaderboards/TotalRating.lua
+++ sea/leaderboards/TotalRating.lua
@@ -34,9 +34,11 @@ function TotalRating:calc(cpvs)
 	local difftable_level = self.difftable_level
 
 	local avg_count = self.avg_count
+	local _avg_count = 0
 
 	for i, cp in ipairs(cpvs) do
 		if i <= avg_count then
+			_avg_count = _avg_count + 1
 			accuracy = accuracy + cp.accuracy
 			rating = rating + cp.rating
 			rating_msd = rating_msd + cp.rating_msd
@@ -46,12 +48,12 @@ function TotalRating:calc(cpvs)
 		rating_pp = rating_pp + cp.rating_pp * 0.95 ^ (i - 1)
 	end
 
-	self.accuracy = accuracy / avg_count
-	self.rating = rating / avg_count
-	self.rating_msd = rating_msd / avg_count
+	self.accuracy = accuracy / _avg_count
+	self.rating = rating / _avg_count
+	self.rating_msd = rating_msd / _avg_count
 	self.rating_pp = rating_pp
-	self.chartmeta_level = chartmeta_level / avg_count
-	self.difftable_level = difftable_level / avg_count
+	self.chartmeta_level = chartmeta_level / _avg_count
+	self.difftable_level = difftable_level / _avg_count
 end
 
 ---@param rating_calc sea.RatingCalc
