ea521c23440a41e4130f526ebc2d7031d0d87843
Thetan <83221369+Thetan-ILW@users.noreply.github.com>
2025-05-25 18:41:38 +0300

    Fix JudgementView, adjust judge animation

--- sphere/views/GameplayView/JudgementView.lua
+++ sphere/views/GameplayView/JudgementView.lua
@@ -6,36 +6,46 @@ local flux = require("flux")
 ---@field game sphere.GameController
 ---@field animate boolean?
 ---@field scale number
----@field judgements sphere.ImageAnimationView
----@field counterIndex {[string]: number}
+---@field judgements sphere.ImageAnimationView[]
+---@field judgesSource sphere.ScoreSystem
 local JudgementView = class()
 
 function JudgementView:load()
-	local score_engine = self.game.rhythmModel.scoreEngine
-	if not score_engine.loaded then
-		return
-	end
+	self.scoreEngine = self.game.rhythmModel.scoreEngine
 
-	self.judge = score_engine:getJudge()
+	local judges_source = self.scoreEngine.judgesSource
+	assert(judges_source)
+	self.totalJudges = #judges_source:getJudges()
+	---@cast judges_source +sphere.ScoreSystem, -sphere.IJudgesSource
+	self.judgesSource = judges_source
+
+	local base_score = self.scoreEngine:getScoreSystem("base")
+	assert(base_score)
+	---@cast base_score sphere.BaseScore
+	self.baseScore = base_score
+
 	self.notes = 0
 
-	self.counterIndex = {}
-
-	local counters = self.judge.orderedCounters
-	for i, v in ipairs(counters) do
-		self.counterIndex[v] = i
-	end
-
-	self.counterIndex.miss = #self.judgements
 	self.scale = self.scale or 1
 	self.animationScale = 1
 	self.rotation = 0
 	self.alpha = 1
 end
 
-function JudgementView:startAnimation()
-	local counter = self.judge.lastCounter
+---@return integer?
+function JudgementView:getJudge()
+	local sequence = self.scoreEngine.sequence
+	local sequence_len = #sequence
 
+	if sequence_len == 0 then
+		return
+	end
+
+	local slice = sequence[sequence_len][self.judgesSource:getKey()]
+	return slice.last_judge
+end
+
+function JudgementView:startAnimation()
 	if self.alphaTween then
 		self.alphaTween:stop()
 	end
@@ -47,29 +57,32 @@ function JudgementView:startAnimation()
 	end
 
 	self.alpha = 1
-	self.alphaTween = flux.to(self, 0.04, { alpha = 0 }):delay(0.18):ease("quadout")
+	self.alphaTween = flux.to(self, 0.08, { alpha = 0 }):delay(0.36):ease("quadout")
 
 	self.rotation = 0
 
-	if counter == "miss" then
+	if self:getJudge() == self.totalJudges then
 		self.animationScale = 1.2
 		self.scaleTween = flux.to(self, 0.1, { animationScale = 1 }):ease("quadout")
 		self.rotationTween = flux.to(self, 0.1, { rotation = (0.2 - math.random(1, 3) * 0.1) }):ease("quadout")
 	else
 		self.animationScale = 0.95
-		self.scaleTween = flux.to(self, 0.04, { animationScale = 1 })
-			:after(self, 0.04, { animationScale = 0.92 })
-			:after(self, 0.1, {})
-			:after(self, 0.04, { animationScale = 0.85 }):ease("quadin")
+		self.scaleTween = flux.to(self, 0.08, { animationScale = 1 })
+			:after(self, 0.08, { animationScale = 0.92 })
+			:after(self, 0.2, {})
+			:after(self, 0.08, { animationScale = 0.85 }):ease("quadin")
 	end
 end
 
 ---@param dt number
 function JudgementView:update(dt)
-	local judge = self.judge
-	local counter_index = self.counterIndex[judge.lastCounter]
+	local judge = self:getJudge()
 
-	local image = self.judgements[counter_index] or self.judgements[1]
+	if not judge then
+		return
+	end
+
+	local image = self.judgements[judge] or self.judgements[1]
 	if not image then
 		return
 	end
@@ -79,7 +92,7 @@ function JudgementView:update(dt)
 	image.sy = self.scale * self.animationScale
 	image.rotation = self.rotation
 
-	local notes = judge.notes
+	local notes = self.baseScore.hitCount + self.baseScore.missCount
 	if notes == self.notes then
 		return
 	end
