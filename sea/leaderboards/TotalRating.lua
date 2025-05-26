71c37955b5bc029e5805fef4f3484ce6938dfb69
semyon422 <semyon422@users.noreply.github.com>
2025-05-26 23:34:19 +0500

    Fix nan rating

--- sea/leaderboards/TotalRating.lua
+++ sea/leaderboards/TotalRating.lua
@@ -48,6 +48,10 @@ function TotalRating:calc(cpvs)
 		rating_pp = rating_pp + cp.rating_pp * 0.95 ^ (i - 1)
 	end
 
+	if _avg_count == 0 then
+		_avg_count = 1
+	end
+
 	self.accuracy = accuracy / _avg_count
 	self.rating = rating / _avg_count
 	self.rating_msd = rating_msd / _avg_count
