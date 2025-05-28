700b52f167d4ddbc0813b89e60873041bd921a5d
semyon422 <semyon422@users.noreply.github.com>
2025-05-28 16:47:35 +0500

    Add migrate cli command

--- sea/app/cli.lua
+++ sea/app/cli.lua
@@ -269,6 +269,25 @@ function cmds.list()
 	end
 end
 
+function cmds.migrate()
+	local ServerMigration = require("sea.storage.old_server.ServerMigration")
+	local LjsqliteDatabase = require("rdb.db.LjsqliteDatabase")
+	local RestyMysqlDatabase = require("rdb.db.RestyMysqlDatabase")
+	local ServerSqliteDatabase = require("sea.storage.server.ServerSqliteDatabase")
+
+	local src_db = RestyMysqlDatabase()
+	src_db:open("backend", "username", "password", "127.0.0.1", 3306)
+
+	local dst_db = ServerSqliteDatabase(LjsqliteDatabase())
+	dst_db.path = "server.db"
+	dst_db:open()
+
+	dst_db.db:exec("BEGIN")
+	local mig = ServerMigration(src_db, dst_db.db)
+	mig:migrate()
+	dst_db.db:exec("COMMIT")
+end
+
 local command = arg[1]
 local f = cmds[command]
 if f then
