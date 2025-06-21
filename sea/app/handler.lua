8a9737c9bc2875c4e5c24df632bc25cf1418264a
semyon422 <semyon422@users.noreply.github.com>
2025-06-21 19:21:04 +0500

    Init app in handler function

--- sea/app/handler.lua
+++ sea/app/handler.lua
@@ -2,14 +2,21 @@ local App = require("sea.app.App")
 
 local app_config = require("app_config")
 
-local app = App(app_config)
+---@type sea.App
+local app
 
-app:load()
+local function init()
+	app = App(app_config)
+	app:load()
+end
 
 ---@param req web.IRequest
 ---@param res web.IResponse
 ---@param ip string
 local function handler(req, res, ip)
+	if not app then
+		init()
+	end
 	app:handle(req, res, ip)
 end
 
