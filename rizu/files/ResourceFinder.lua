12c0301d2da0aa50442618998ec96752dc8a653a
semyon422 <semyon422@users.noreply.github.com>
2025-08-13 13:34:21 +0500

    Add ResourceFinder class

---
+++ rizu/files/ResourceFinder.lua
@@ -0,0 +1,107 @@
+local class = require("class")
+local path_util = require("path_util")
+
+---@alias rizu.ResourceFormat "audio"|"image"|"video"
+
+---@type {[rizu.ResourceFormat]: string[]}
+local format_extensions = {
+	audio = {"wav", "ogg", "mp3"},
+	image = {"png", "bmp", "jpg"},
+	video = {"mpg", "avi", "mp4", "mpeg", "wmv"},
+}
+
+---@type {[string]: rizu.ResourceFormat}
+local extension_format = {}
+for format, list in pairs(format_extensions) do
+	for i = 1, #list do
+		extension_format[list[i]] = format
+	end
+end
+
+---@class rizu.ResourceFinder
+---@operator call: rizu.ResourceFinder
+local ResourceFinder = class()
+
+---@param fs fs.IFilesystem
+function ResourceFinder:new(fs)
+	self.fs = fs
+	self:reset()
+end
+
+function ResourceFinder:reset()
+	---@type string[]
+	self.paths = {}
+	---@type {[string]: string[]}
+	self.path_files = {}
+end
+
+---@param path string
+function ResourceFinder:addPath(path)
+	table.insert(self.paths, path)
+	self.path_files[path] = self:getFilesRecursive(path)
+end
+
+---@param path string
+---@param list table?
+---@param prefix string?
+---@return table
+function ResourceFinder:getFilesRecursive(path, list, prefix)
+	list = list or {}
+	prefix = prefix or ""
+	for _, file in ipairs(self.fs:getDirectoryItems(path)) do
+		local info = assert(self.fs:getInfo(path .. "/" .. file))
+		if info.type == "directory" then
+			self:getFilesRecursive(path .. "/" .. file, list, prefix .. file .. "/")
+		else
+			table.insert(list, prefix .. file)
+		end
+	end
+	return list
+end
+
+---@param ext string?
+---@return rizu.ResourceFormat?
+function ResourceFinder:getFormat(ext)
+	return ext and extension_format[ext:lower()]
+end
+
+---@param req_name_ext string
+---@param format string
+---@return string?
+function ResourceFinder:findFile(req_name_ext, format)
+	req_name_ext = path_util.fix_separators(req_name_ext)
+	req_name_ext = req_name_ext:gsub("^/", "")
+
+	local req_name, req_ext = path_util.name_ext(req_name_ext)
+	if self:getFormat(req_ext) ~= assert(format) then
+		return
+	end
+
+	local extensions = format_extensions[format]
+
+	for _, path in ipairs(self.paths) do
+		local req_path = path .. "/" .. req_name_ext
+		if self.fs:getInfo(req_path) then
+			return req_path
+		end
+
+		local files = self.path_files[path]
+
+		for _, name_ext in ipairs(files) do
+			if name_ext:lower() == req_name_ext:lower() then
+				return path .. "/" .. name_ext
+			end
+		end
+
+		for _, check_ext in ipairs(extensions) do
+			for _, name_ext in ipairs(files) do
+				local _name, _ext = path_util.name_ext(name_ext)
+				if _ext and _name:lower() == req_name:lower() and _ext:lower() == check_ext then
+					return path .. "/" .. name_ext
+				end
+			end
+		end
+	end
+end
+
+return ResourceFinder
---
+++ rizu/files/ResourceFinder_test.lua
@@ -0,0 +1,78 @@
+local ResourceFinder = require("rizu.files.ResourceFinder")
+local FakeFilesystem = require("fs.FakeFilesystem")
+
+local test = {}
+
+---@param t testing.T
+function test.find_file(t)
+	local fs = FakeFilesystem()
+	local rf = ResourceFinder(fs)
+
+	fs:createDirectory("dir")
+	fs:write("dir/audio.mp3", "")
+
+	rf:addPath("dir")
+
+	t:eq(rf:findFile("audio.mp3", "audio"), "dir/audio.mp3")
+	t:eq(rf:findFile("/audio.mp3", "audio"), "dir/audio.mp3")
+	t:eq(rf:findFile("\\audio.mp3", "audio"), "dir/audio.mp3")
+end
+
+---@param t testing.T
+function test.equal_name_found(t)
+	local fs = FakeFilesystem()
+	local rf = ResourceFinder(fs)
+
+	fs:createDirectory("dir")
+	fs:write("dir/Audio.MP3", "")
+	fs:write("dir/audio.mp3", "")
+
+	rf:addPath("dir")
+
+	t:eq(rf:findFile("audio.mp3", "audio"), "dir/audio.mp3")
+end
+
+---@param t testing.T
+function test.case_insensetive(t)
+	local fs = FakeFilesystem()
+	local rf = ResourceFinder(fs)
+
+	fs:createDirectory("dir")
+	fs:write("dir/audio.mp3", "")
+
+	rf:addPath("dir")
+
+	t:eq(rf:findFile("AUDIO.MP3", "audio"), "dir/audio.mp3")
+end
+
+---@param t testing.T
+function test.alternate_ext_ordered(t)
+	local fs = FakeFilesystem()
+	local rf = ResourceFinder(fs)
+
+	fs:createDirectory("dir")
+	fs:write("dir/audio.wav", "")
+	fs:write("dir/audio.mp3", "")
+
+	rf:addPath("dir")
+
+	t:eq(rf:findFile("audio.ogg", "audio"), "dir/audio.wav")
+end
+
+---@param t testing.T
+function test.many_paths(t)
+	local fs = FakeFilesystem()
+	local rf = ResourceFinder(fs)
+
+	fs:createDirectory("dir1")
+	fs:createDirectory("dir2")
+	fs:write("dir1/audio.mp3", "")
+	fs:write("dir2/audio.wav", "")
+
+	rf:addPath("dir1")
+	rf:addPath("dir2")
+
+	t:eq(rf:findFile("audio.ogg", "audio"), "dir1/audio.mp3")
+end
+
+return test
