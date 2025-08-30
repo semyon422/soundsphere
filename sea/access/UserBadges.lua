71845c6e722c714536068e931ba2dcb3377912fd
Nimue <83221369+Nimue-lua@users.noreply.github.com>
2025-08-30 19:12:46 +0300

    Add pages to create/delete badges

--- sea/access/UserBadges.lua
+++ sea/access/UserBadges.lua
@@ -1,4 +1,6 @@
 local class = require("class")
+local UserBadge = require("sea.access.UserBadge")
+local UsersAccess = require("sea.access.access.UsersAccess")
 
 ---@class sea.UserBadges
 ---@operator call: sea.UserBadges
@@ -7,12 +9,53 @@ local UserBadges = class()
 ---@param users_repo sea.UsersRepo
 function UserBadges:new(users_repo)
 	self.users_repo = users_repo
+	self.users_access = UsersAccess()
 end
 
 ---@param user sea.User
 ---@return sea.UserBadge[]
 function UserBadges:getUserBadges(user)
 	return self.users_repo:getUserBadges(user.id)
+end
+
+---@param user sea.User
+---@param target_user_id integer
+---@param badge_id string
+---@return sea.UserBadge
+---@return string err
+function UserBadges:createUserBadge(user, target_user_id, badge_id)
+	if not UserBadge:encode_safe(badge_id) then
+		return nil, ("badge doesn't exist")
+	end
+
+	local target_user = self.users_repo:getUser(target_user_id)
+	if not target_user then
+		return nil, "not found"
+	end
+
+	local can, err = self.users_access:canUpdate(user, target_user, time)
+	if not can then
+		return nil, "not allowed"
+	end
+
+	return self.users_repo:createUserBadge(target_user.id, badge_id)
+end
+
+---@param user sea.User
+---@param target_user_id integer
+---@param badge_id string
+function UserBadges:deleteUserBadge(user, target_user_id, badge_id)
+	local target_user = self.users_repo:getUser(target_user_id)
+	if not target_user then
+		return nil, "not found"
+	end
+
+	local can, err = self.users_access:canUpdate(user, target_user, time)
+	if not can then
+		return nil, "not allowed"
+	end
+
+	return self.users_repo:deleteUserBadge(target_user_id, badge_id)
 end
 
 return UserBadges
---
+++ sea/access/user_badge_list.lua
@@ -0,0 +1,15 @@
+---@type {[string]: {name: string, description: string, leaderboard_id: integer?}
+local t = {
+	summer_event_2025_winner = {
+		name = "Summer 2025 - Winner",
+		description = "Winner of the 2025 full combo summer event.",
+		leaderboard_id = 8,
+	},
+	summer_event_2025_top_active = {
+		name = "Summer 2025 - Top Active",
+		description = "The most active player during the 2025 full combo summer event.",
+		leaderboard_id = 8,
+	},
+}
+
+return t
