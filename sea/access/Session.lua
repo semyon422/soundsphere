8f3458a063e1883b3457ec7627e7ad15f76c008c
semyon422 <semyon422@users.noreply.github.com>
2026-02-15 22:59:02 +0500

    Add validation for Session

--- sea/access/Session.lua
+++ sea/access/Session.lua
@@ -1,4 +1,6 @@
 local class = require("class")
+local valid = require("valid")
+local types = require("sea.shared.types")
 
 ---@class sea.Session
 ---@operator call: sea.Session
@@ -8,6 +10,22 @@ local class = require("class")
 ---@field created_at integer
 ---@field updated_at integer
 local Session = class()
+
+Session.struct = {
+	id = types.integer,
+	user_id = types.integer,
+	active = types.boolean,
+	created_at = types.integer,
+	updated_at = types.integer,
+}
+
+local validate_session = valid.struct(Session.struct)
+
+---@return true?
+---@return string?
+function Session:validate()
+	return valid.format(validate_session(self))
+end
 
 ---@return boolean
 function Session:isAnon()
