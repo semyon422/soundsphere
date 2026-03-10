908e5940d3e3944d5211d8e1c693b78d74ea4475
Nimue <83221369+Nimue-lua@users.noreply.github.com>
2026-03-10 09:46:57 +0300

    Block input propagation if modal is active

--- yi/views/Modals.lua
+++ yi/views/Modals.lua
@@ -10,24 +10,28 @@ function Modals:new()
 	View.new(self)
 	self:setWidth("100%")
 	self:setHeight("100%")
-	self.handles_keyboard_input = true
 end
 
----@param m function
+---@param m function?
 function Modals:setImguiModal(m)
 	imgui_ctx.game = self:getGame()
 
-	if self.imgui_modal == m then
+	if m == nil or self.imgui_modal == m then
 		self.imgui_modal = nil
+		self.handles_keyboard_input = false
+		self.handles_mouse_input = false
 	else
 		self.imgui_modal = m
+		self.handles_keyboard_input = true
+		self.handles_mouse_input = true
 	end
 end
 
 function Modals:onKeyDown(e)
 	if e.key == "escape" then
-		self.imgui_modal = nil
+		self:setImguiModal()
 	end
+	return true
 end
 
 function Modals:draw()
